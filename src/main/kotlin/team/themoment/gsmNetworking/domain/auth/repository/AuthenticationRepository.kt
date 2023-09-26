42adf3b68254c3e76e87868ec12e14b8bfdb6ae8
SungGil-5125 <82089918+SungGil-5125@users.noreply.github.com>
2023-09-26 17:15:25 +0900

    :recycle: id 타입 수정 및 findByProviderId 삭제

--- src/main/kotlin/team/themoment/gsmNetworking/domain/auth/repository/AuthenticationRepository.kt
+++ src/main/kotlin/team/themoment/gsmNetworking/domain/auth/repository/AuthenticationRepository.kt
@@ -6,9 +6,8 @@ import org.springframework.data.repository.CrudRepository
 /**
  * Authentication Entity를 위한 Repository 인터페이스 입니다.
  */
-interface AuthenticationRepository: CrudRepository<Authentication, String> {
+interface AuthenticationRepository: CrudRepository<Authentication, Long> {
 
     fun findByEmail(email: String): Authentication?
-    fun findByProviderId(providerId: String): Authentication?
 
 }
