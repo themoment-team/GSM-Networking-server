6d3db51f824322725562ca089f3f4c464c4fe878
SungGil-5125 <82089918+SungGil-5125@users.noreply.github.com>
2023-09-26 10:49:25 +0900

    :recycle: 도메인에서 사용하는 exception 클래스들 common 패키지로 이전

---
+++ src/main/kotlin/team/themoment/gsmNetworking/common/exception/ExpectedException.kt
@@ -0,0 +1,11 @@
+package team.themoment.gsmNetworking.common.exception
+
+import org.springframework.http.HttpStatus
+
+/**
+ * 클라이언트에게 예외 상황에 HttpStatus와 Message를 전달해주기 위해 사용되는 클래스 입니다.
+ */
+class ExpectedException(
+    override val message: String,
+    val status: HttpStatus,
+): RuntimeException(message)
---
+++ src/main/kotlin/team/themoment/gsmNetworking/common/exception/model/ExceptionResponse.kt
@@ -0,0 +1,8 @@
+package team.themoment.gsmNetworking.common.exception.model
+
+/**
+ * Exception 발생시 클라이언트에게 반환 할 Response 클래스 입니다.
+ */
+class ExceptionResponse(
+    val message: String
+)
