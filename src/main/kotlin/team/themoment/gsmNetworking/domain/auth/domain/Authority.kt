81e1cbc07940172ab287ae39e23ffadea31d5e8d
SungGil-5125 <82089918+SungGil-5125@users.noreply.github.com>
2023-09-26 17:15:46 +0900

    :fire: 역할을 나타내는 ROLE_ prefix 삭제

--- src/main/kotlin/team/themoment/gsmNetworking/domain/auth/domain/Authority.kt
+++ src/main/kotlin/team/themoment/gsmNetworking/domain/auth/domain/Authority.kt
@@ -3,13 +3,12 @@ package team.themoment.gsmNetworking.domain.auth.domain
 /**
  * 사용자의 권한을 판별하는 enum 클래스 입니다.
  */
-enum class Authority(
-    val authority: String
-) {
+enum class Authority {
 
-    ROLE_UNAUTHENTICATED("UNAUTHENTICATED"),
-    ROLE_TEMP_USER("TEMP_USER"),
-    ROLE_USER("USER"),
-    ROLE_ADMIN("ADMIN")
+    UNAUTHENTICATED,
+    TEMP_USER,
+    USER,
+    ADMIN
+    ;
 
 }
