4682842f8e0dfad9e99a3ead4d703f9e43f9d3fa
SungGil-5125 <82089918+SungGil-5125@users.noreply.github.com>
2023-09-26 10:48:19 +0900

    :sparkles: User 서비스 모놀리식으로 전환

---
+++ src/main/kotlin/team/themoment/gsmNetworking/domain/mentor/repository/CareerRepository.kt
@@ -0,0 +1,6 @@
+package team.themoment.gsmNetworking.domain.mentor.repository
+
+import team.themoment.gsmNetworking.domain.mentor.domain.Career
+import org.springframework.data.repository.CrudRepository
+
+interface CareerRepository: CrudRepository<Career, Long>
