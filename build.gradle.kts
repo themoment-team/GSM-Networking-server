e71c1e7494fa69bdaaccc3d930972217d1e09514
YangSiJun528 <yangsijun5528@gmail.com>
2023-09-27 12:23:56 +0900

    :heavy_plus_sign: spring-boot-starter-websocket 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -68,6 +68,9 @@ dependencies {
 	implementation("io.jsonwebtoken:jjwt-api:0.11.5")
 	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
 	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
+
+	/* websocket */
+	implementation("org.springframework.boot:spring-boot-starter-websocket")
 }
 
 tasks.withType<KotlinCompile> {
