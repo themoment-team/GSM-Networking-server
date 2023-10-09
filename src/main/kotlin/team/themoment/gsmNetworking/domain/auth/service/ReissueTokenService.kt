9e5c4caded3a01e2c3bc0e5321823241cd7e59fc
SungGil-5125 <82089918+SungGil-5125@users.noreply.github.com>
2023-10-09 16:32:37 +0900

    :recycle: 헤더 -> 쿠키를 요청 받도록 수정

--- src/main/kotlin/team/themoment/gsmNetworking/domain/auth/service/ReissueTokenService.kt
+++ src/main/kotlin/team/themoment/gsmNetworking/domain/auth/service/ReissueTokenService.kt
@@ -1,33 +1,27 @@
 package team.themoment.gsmNetworking.domain.auth.service
 
-import org.springframework.data.repository.findByIdOrNull
 import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
-import team.themoment.gsmNetworking.common.cookie.CookieUtil
 import team.themoment.gsmNetworking.common.exception.ExpectedException
 import team.themoment.gsmNetworking.domain.auth.domain.RefreshToken
-import team.themoment.gsmNetworking.domain.auth.repository.AuthenticationRepository
 import team.themoment.gsmNetworking.domain.auth.repository.RefreshTokenRepository
 import team.themoment.gsmNetworking.global.security.jwt.TokenGenerator
-import team.themoment.gsmNetworking.global.security.jwt.TokenParser
+import team.themoment.gsmNetworking.global.security.jwt.dto.TokenDto
 import team.themoment.gsmNetworking.global.security.jwt.properties.JwtExpTimeProperties
-import javax.servlet.http.HttpServletResponse
 
 /**
  * 로그인 연장을 위한 토큰 재발급을 해주는 서비스 클래스 입니다.
  */
 @Service
 class ReissueTokenService(
-    private val tokenParser: TokenParser,
     private val tokenGenerator: TokenGenerator,
     private val refreshTokenRepository: RefreshTokenRepository,
-    private val authenticationRepository: AuthenticationRepository,
     private val jwtExpTimeProperties: JwtExpTimeProperties
 ) {
 
     /**
-     * 유효한 RefreshToken 이라면 토큰을 쿠키에 저장하고, 유효하지 않은 RefreshToken 이라면 예외를 던집니다.
+     * 유효한 RefreshToken 이라면 토큰을 반환하고, 유효하지 않은 RefreshToken 이라면 예외를 던집니다.
      *
      * @param token 로그인 연장에 필요한 재발급(RefreshToken) 토큰
      * @throws ExpectedException 이 터지는 조건은 아래와 같다.
@@ -36,15 +30,13 @@ class ReissueTokenService(
      *      3. 유효하지 않은 Token Prefix로 요청 한 경우
      */
     @Transactional(rollbackFor = [Exception::class])
-    fun execute(token: String, response: HttpServletResponse) {
-        val parsedToken = tokenParser.parseRefreshToken(token)
-        val refreshToken = refreshTokenRepository.findByToken(parsedToken)
+    fun execute(token: String?): TokenDto {
+        if (token == null) throw ExpectedException("refresh token 쿠키를 가지고 있지 않은 사용자 입니다.", HttpStatus.NOT_FOUND)
+        val refreshToken = refreshTokenRepository.findByToken(token)
             ?: throw ExpectedException("존재하지 않는 refresh token 입니다.", HttpStatus.NOT_FOUND)
-        val authentication = authenticationRepository.findByIdOrNull(refreshToken.authenticationId)
-            ?: throw ExpectedException("refresh token subject인 email을 찾을 수 없습니다.", HttpStatus.NOT_FOUND)
         val tokenDto = tokenGenerator.generateToken(refreshToken.authenticationId)
-        CookieUtil.addTokenCookie(tokenDto, response)
-        saveRefreshToken(tokenDto.refreshToken, authentication.authenticationId)
+        saveRefreshToken(tokenDto.refreshToken, refreshToken.authenticationId)
+        return tokenDto
     }
 
     /**
--- src/main/kotlin/team/themoment/gsmNetworking/global/security/jwt/TokenParser.kt
+++ src/main/kotlin/team/themoment/gsmNetworking/global/security/jwt/TokenParser.kt
@@ -2,10 +2,8 @@ package team.themoment.gsmNetworking.global.security.jwt
 
 import io.jsonwebtoken.Claims
 import io.jsonwebtoken.Jwts
-import org.springframework.http.HttpStatus
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.stereotype.Component
-import team.themoment.gsmNetworking.common.exception.ExpectedException
 import team.themoment.gsmNetworking.global.security.jwt.properties.JwtProperties
 import team.themoment.gsmNetworking.global.security.principal.AuthenticationDetailsService
 import java.security.Key
@@ -18,20 +16,6 @@ class TokenParser(
     private val authenticationDetailsService: AuthenticationDetailsService,
     private val jwtProperties: JwtProperties
 ) {
-
-    /**
-     * 재발급 토큰의 prefix를 파싱하는 메서드 입니다.
-     *
-     * @param refreshToken prefix를 포함한 refreshToken 입니다.
-     * @throws ExpectedException 이 터지는 조건은 아래와 같다.
-     *       1. jwt의 토큰 prefix인 Bearer이 붙지 않은 경우
-     * @return prefix를 replace한 refreshToken을 반환 합니다.
-     */
-    fun parseRefreshToken(refreshToken: String): String {
-        if (refreshToken.startsWith(JwtProperties.TOKEN_PREFIX))
-            return refreshToken.replace(JwtProperties.TOKEN_PREFIX, "")
-        throw ExpectedException("유효하지 않은 refresh token 입니다.", HttpStatus.UNAUTHORIZED)
-    }
 
     /**
      * 토큰의 값으로 authentication 객체를 만드는 메서드 입니다.
