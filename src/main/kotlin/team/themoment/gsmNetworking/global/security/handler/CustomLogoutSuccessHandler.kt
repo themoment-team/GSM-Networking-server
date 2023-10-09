745e2a0af45f2843c26caa32f2fec48a8af7446a
SungGil-5125 <82089918+SungGil-5125@users.noreply.github.com>
2023-10-09 14:51:59 +0900

    :recycle: response status 200 -> 205 로 수정

--- src/main/kotlin/team/themoment/gsmNetworking/global/security/handler/CustomLogoutSuccessHandler.kt
+++ src/main/kotlin/team/themoment/gsmNetworking/global/security/handler/CustomLogoutSuccessHandler.kt
@@ -1,7 +1,6 @@
 package team.themoment.gsmNetworking.global.security.handler
 
 import com.fasterxml.jackson.databind.ObjectMapper
-import team.themoment.gsmNetworking.domain.auth.repository.RefreshTokenRepository
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
@@ -9,6 +8,7 @@ import org.springframework.security.core.Authentication
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler
 import org.springframework.stereotype.Component
 import team.themoment.gsmNetworking.common.exception.ExpectedException
+import team.themoment.gsmNetworking.domain.auth.repository.RefreshTokenRepository
 import team.themoment.gsmNetworking.global.security.oauth.properties.Oauth2Properties
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
@@ -23,22 +23,34 @@ class CustomLogoutSuccessHandler(
 ): LogoutSuccessHandler {
 
     /**
-     * 로그아웃 성공 후 Redis에 저장되어있던 재발급 토큰을 삭제 시킵니다.
+     * authentication 객체가 있다면 redis에 저장되어있던 재발급 토큰을 삭제 시킵니다.
+     *
+     * @param request 요청 받은 servletRequest
+     * @param response 응답할 servletResponse
+     * @param authentication 생성된 인증 객체
+     * @throws ExpectedException 이 터지는 조건은 아래와 같다.
+     *      1. RefreshToken Entity에 저장되지 않은 RefreshToken일 경우
      */
     override fun onLogoutSuccess(
         request: HttpServletRequest,
         response: HttpServletResponse,
         authentication: Authentication?,
     ) {
-        response.status = HttpServletResponse.SC_OK
         if (authentication != null) {
             val refreshToken = refreshTokenRepository.findByIdOrNull(authentication.name)
                 ?: throw ExpectedException("존재하지 않는 refresh token 입니다.", HttpStatus.NOT_FOUND)
             refreshTokenRepository.delete(refreshToken)
-        } else sendErrorResponse(response)
-        response.sendRedirect(oauth2Properties.defaultRedirectUrl)
+            response.status = HttpServletResponse.SC_RESET_CONTENT
+            response.sendRedirect(oauth2Properties.defaultRedirectUrl)
+        }
+        sendErrorResponse(response)
     }
 
+    /**
+     * authentication 객체가 존재하지 않을 경우 에러 메세지를 반환하는 메서드 입니다.
+     *
+     * @param response 응답할 serveltResponse
+     */
     private fun sendErrorResponse(response: HttpServletResponse) {
         response.status = HttpServletResponse.SC_BAD_REQUEST
         response.contentType = MediaType.APPLICATION_JSON_VALUE
