23dce6a815b1c39521303a93c41009125b6a7057
SungGil-5125 <82089918+SungGil-5125@users.noreply.github.com>
2023-10-09 14:52:39 +0900

    :sparkles: 로그아웃 전 커스텀 토큰 필터 등록

--- src/main/kotlin/team/themoment/gsmNetworking/global/filter/config/FilterConfig.kt
+++ src/main/kotlin/team/themoment/gsmNetworking/global/filter/config/FilterConfig.kt
@@ -5,6 +5,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.oauth2.client.web.OAuth2LoginAuthenticationFilter
 import org.springframework.security.web.DefaultSecurityFilterChain
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+import org.springframework.security.web.authentication.logout.LogoutFilter
 import org.springframework.stereotype.Component
 import team.themoment.gsmNetworking.global.filter.ExceptionHandlerFilter
 import team.themoment.gsmNetworking.global.filter.TokenRequestFilter
@@ -16,6 +17,7 @@ class FilterConfig(
 ): SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity>() {
 
     override fun configure(builder: HttpSecurity) {
+        builder.addFilterBefore(tokenRequestFilter, LogoutFilter::class.java)
         builder.addFilterBefore(tokenRequestFilter, UsernamePasswordAuthenticationFilter::class.java)
         builder.addFilterBefore(exceptionHandlerFilter, OAuth2LoginAuthenticationFilter::class.java)
     }
