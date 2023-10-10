e1cd7a0063d9f0cda644e83ec0850545e6661807
SungGil-5125 <sunggil0125@naver.com>
2023-10-10 13:05:06 +0900

    :sparkles: 멘토 전체 리스트 반환 구현

--- src/main/kotlin/team/themoment/gsmNetworking/domain/mentor/controller/MentorController.kt
+++ src/main/kotlin/team/themoment/gsmNetworking/domain/mentor/controller/MentorController.kt
@@ -2,23 +2,33 @@ package team.themoment.gsmNetworking.domain.mentor.controller
 
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import team.themoment.gsmNetworking.domain.mentor.dto.MentorInfoDto
 import team.themoment.gsmNetworking.domain.mentor.dto.MentorRegistrationDto
 import team.themoment.gsmNetworking.domain.mentor.service.MentorRegistrationService
+import team.themoment.gsmNetworking.domain.mentor.service.QueryMentorListService
 
 @RestController
 @RequestMapping("api/v1/mentor")
 class MentorController(
-    private val mentorRegistrationService: MentorRegistrationService
+    private val mentorRegistrationService: MentorRegistrationService,
+    private val queryMentorListService: QueryMentorListService
 ) {
 
     @PostMapping
     fun saveMentorInfo(@RequestBody dto: MentorRegistrationDto): ResponseEntity<Void> {
         mentorRegistrationService.execute(dto)
         return ResponseEntity.status(HttpStatus.CREATED).build()
+    }
+
+    @GetMapping
+    fun queryMentorList(): ResponseEntity<List<MentorInfoDto>> {
+        val mentorList = queryMentorListService.execute()
+        return ResponseEntity.ok(mentorList)
     }
 
 }
