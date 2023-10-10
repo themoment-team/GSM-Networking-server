b2046dbd8816b312f6fe54b8e0bea4f54eb2a6d7
SungGil-5125 <sunggil0125@naver.com>
2023-10-10 17:16:37 +0900

    :sparkles: 기존 json 필드 사용하도록 jsonProperty 추가

--- src/main/kotlin/team/themoment/gsmNetworking/domain/mentor/dto/MentorInfoDto.kt
+++ src/main/kotlin/team/themoment/gsmNetworking/domain/mentor/dto/MentorInfoDto.kt
@@ -1,5 +1,7 @@
 package team.themoment.gsmNetworking.domain.mentor.dto
 
+import com.fasterxml.jackson.annotation.JsonProperty
+
 data class MentorInfoDto(
     val id: Long,
     val name: String,
@@ -7,12 +9,14 @@ data class MentorInfoDto(
     val generation: Int,
     val position: String,
     val company: CompanyInfo,
+    @field:JsonProperty("SNS")
     val sns: String?,
     val profileUrl: String?
 ) {
 
     data class CompanyInfo(
         val name: String,
+        @field:JsonProperty("URL")
         val url: String
     )
 
