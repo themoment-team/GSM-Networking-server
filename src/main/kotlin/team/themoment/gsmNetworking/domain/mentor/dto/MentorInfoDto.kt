f48e92f0f4bb7dd0099ec88410d2bdc2e03f6846
SungGil-5125 <sunggil0125@naver.com>
2023-10-10 17:13:39 +0900

    :sparkles: profileUrl 추가

--- src/main/kotlin/team/themoment/gsmNetworking/domain/mentor/dto/MentorInfoDto.kt
+++ src/main/kotlin/team/themoment/gsmNetworking/domain/mentor/dto/MentorInfoDto.kt
@@ -1,18 +1,19 @@
 package team.themoment.gsmNetworking.domain.mentor.dto
 
 data class MentorInfoDto(
-    private val id: Long,
-    private val name: String,
-    private val email: String,
-    private val generation: Int,
-    private val position: String,
-    private val company: CompanyInfo,
-    private val sns: String
+    val id: Long,
+    val name: String,
+    val email: String,
+    val generation: Int,
+    val position: String,
+    val company: CompanyInfo,
+    val sns: String?,
+    val profileUrl: String?
 ) {
 
     data class CompanyInfo(
-        private val name: String,
-        private val url: String
+        val name: String,
+        val url: String
     )
 
 }
