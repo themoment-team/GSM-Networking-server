8e0954a2b38ceaeff1d8c2918e3d7a4bc33c17e4
YangSiJun528 <yangsijun5528@gmail.com>
2023-10-11 20:33:50 +0900

    :sparkles: ChatType 별 지원 클래스 추가

--- src/main/kotlin/team/themoment/gsmNetworking/domain/chat/domain/ChatType.kt
+++ src/main/kotlin/team/themoment/gsmNetworking/domain/chat/domain/ChatType.kt
@@ -1,6 +1,7 @@
 package team.themoment.gsmNetworking.domain.chat.domain
 
-enum class ChatType {
-    USER,
-    SYSTEM
+enum class ChatType(clazz: Class<out BaseChat>) {
+    ALL(BaseChat::class.java),
+    USER(UserChat::class.java),
+    SYSTEM(SystemChat::class.java)
 }
