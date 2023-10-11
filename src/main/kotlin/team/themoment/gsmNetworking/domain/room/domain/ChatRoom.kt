55ed4e0f61fdacb2369479a781941e18a28e867e
YangSiJun528 <yangsijun5528@gmail.com>
2023-10-11 18:06:07 +0900

    :sparkles: ChatRoom 구현

---
+++ src/main/kotlin/team/themoment/gsmNetworking/domain/room/domain/ChatRoom.kt
@@ -0,0 +1,22 @@
+package team.themoment.gsmNetworking.domain.room.domain
+
+import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.index.CompoundIndex
+import org.springframework.data.mongodb.core.index.CompoundIndexes
+import org.springframework.data.mongodb.core.mapping.Document
+import java.util.*
+
+@Document(collection = "room")
+@CompoundIndexes(
+    CompoundIndex(
+        def = "{'userInfos.userId': 1, 'userInfos.lastViewedChatId': -1}",
+        name = "room_user_info_compound_index",
+        unique = true
+    )
+)
+class ChatRoom(
+    @Id
+    val id: UUID,
+    val name: String,
+    val userInfos: List<ChatUserInfo>
+)
