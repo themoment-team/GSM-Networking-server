214b9298463c47771a08fed96bb365f296e93e5b
YangSiJun528 <yangsijun5528@gmail.com>
2023-10-11 18:04:50 +0900

    :sparkles: SystemChat 구현

---
+++ src/main/kotlin/team/themoment/gsmNetworking/domain/chat/domain/SystemChat.kt
@@ -0,0 +1,16 @@
+package team.themoment.gsmNetworking.domain.chat.domain
+
+import org.bson.types.ObjectId
+import org.springframework.data.annotation.TypeAlias
+import org.springframework.data.mongodb.core.mapping.Document
+import team.themoment.gsmNetworking.domain.room.domain.ChatRoom
+
+@Document(collection = "chat")
+@TypeAlias("system")
+class SystemChat(
+    id: ObjectId,
+    room: ChatRoom,
+    val senderId: Long,
+    content: String,
+) : BaseChat(id, room, content) {
+}
