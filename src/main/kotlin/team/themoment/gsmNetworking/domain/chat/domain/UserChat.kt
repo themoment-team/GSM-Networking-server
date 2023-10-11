275c9d8e1f72bdbc091dd8e76a02bab6c2f5cd52
YangSiJun528 <yangsijun5528@gmail.com>
2023-10-11 18:05:46 +0900

    :sparkles: UserChat 구현

---
+++ src/main/kotlin/team/themoment/gsmNetworking/domain/chat/domain/UserChat.kt
@@ -0,0 +1,15 @@
+package team.themoment.gsmNetworking.domain.chat.domain
+
+import org.bson.types.ObjectId
+import org.springframework.data.annotation.TypeAlias
+import org.springframework.data.mongodb.core.mapping.Document
+import team.themoment.gsmNetworking.domain.room.domain.ChatRoom
+
+@Document(collection = "chat")
+@TypeAlias("user")
+class UserChat(
+    id: ObjectId,
+    room: ChatRoom,
+    content: String,
+) : BaseChat(id, room, content) {
+}
