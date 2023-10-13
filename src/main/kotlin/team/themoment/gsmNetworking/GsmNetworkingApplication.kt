82f5c59b20e2030962932bd9a42494518163fde2
YangSiJun528 <yangsijun5528@gmail.com>
2023-10-13 19:42:23 +0900

    :sparkles: `@EnableJpaAuditing` 추가

--- src/main/kotlin/team/themoment/gsmNetworking/GsmNetworkingApplication.kt
+++ src/main/kotlin/team/themoment/gsmNetworking/GsmNetworkingApplication.kt
@@ -2,8 +2,10 @@ package team.themoment.gsmNetworking
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
 @SpringBootApplication
+@EnableJpaAuditing
 class GsmNetworkingApplication
 
 fun main(args: Array<String>) {
