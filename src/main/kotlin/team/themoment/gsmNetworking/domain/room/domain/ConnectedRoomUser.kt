8454feae59d4243fe1f58d72a6cfaa75118e821f
YangSiJun528 <yangsijun5528@gmail.com>
2023-10-13 20:03:46 +0900

    :sparkles: ConnectedRoomUser 구현

---
+++ src/main/kotlin/team/themoment/gsmNetworking/domain/room/domain/ConnectedRoomUser.kt
@@ -0,0 +1,15 @@
+package team.themoment.gsmNetworking.domain.room.domain
+
+import org.springframework.data.redis.core.RedisHash
+import org.springframework.data.redis.core.index.Indexed
+import javax.persistence.*
+
+@RedisHash(value = "connected_room_user")
+class ConnectedRoomUser(
+    @Id
+    val roomId: Long,
+
+    @Indexed
+    val connectedUserId: Set<Long>
+) {
+}
