3c5c5e6d35ddb86eff23a1b60866a3fcdf1ff7a7
YangSiJun528 <yangsijun5528@gmail.com>
2023-10-14 18:57:24 +0900

    :recycle: @EnableJpaAuditing 설정을 `EnableJpaAuditingConfig` 로 분리

--- src/main/kotlin/team/themoment/gsmNetworking/GsmNetworkingApplication.kt
+++ src/main/kotlin/team/themoment/gsmNetworking/GsmNetworkingApplication.kt
@@ -5,7 +5,6 @@ import org.springframework.boot.runApplication
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
 @SpringBootApplication
-@EnableJpaAuditing
 class GsmNetworkingApplication
 
 fun main(args: Array<String>) {
