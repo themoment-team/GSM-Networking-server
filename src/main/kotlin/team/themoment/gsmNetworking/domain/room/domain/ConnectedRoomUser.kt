45cf90b0b7fc97e928dd7606a9e154b7e4f8ee10
YangSiJun528 <yangsijun5528@gmail.com>
2023-10-14 21:20:44 +0900

    :recycle:  `ConnectedRoomUser` data class로 변경하기

--- src/main/kotlin/team/themoment/gsmNetworking/domain/room/domain/ConnectedRoomUser.kt
+++ src/main/kotlin/team/themoment/gsmNetworking/domain/room/domain/ConnectedRoomUser.kt
@@ -1,11 +1,10 @@
 package team.themoment.gsmNetworking.domain.room.domain
 
 import org.springframework.data.redis.core.RedisHash
-import org.springframework.data.redis.core.index.Indexed
 import javax.persistence.*
 
 @RedisHash(value = "connected_room_user")
-class ConnectedRoomUser(
+data class ConnectedRoomUser(
     @Id
     val roomId: Long,
 
