877b8492905ce21f0e459d21f98f6286d1536589
YangSiJun528 <yangsijun5528@gmail.com>
2023-10-14 21:13:19 +0900

    :coffin: 사용하지 않는 import 문 제거

--- src/main/kotlin/team/themoment/gsmNetworking/GsmNetworkingApplication.kt
+++ src/main/kotlin/team/themoment/gsmNetworking/GsmNetworkingApplication.kt
@@ -2,7 +2,6 @@ package team.themoment.gsmNetworking
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
 @SpringBootApplication
 class GsmNetworkingApplication
