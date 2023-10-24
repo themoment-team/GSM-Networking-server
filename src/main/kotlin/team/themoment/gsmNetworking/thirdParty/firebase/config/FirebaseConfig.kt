3b710dfea94e2848e87f80e555a4076f1a21cb85
hajeu <s22072@gsm.hs.kr>
2023-10-24 09:45:57 +0900

    :recycle: firebase key 파일 이름을 환경변수로 관리

--- src/main/kotlin/team/themoment/gsmNetworking/thirdParty/firebase/config/FirebaseConfig.kt
+++ src/main/kotlin/team/themoment/gsmNetworking/thirdParty/firebase/config/FirebaseConfig.kt
@@ -3,16 +3,20 @@ package team.themoment.gsmNetworking.thirdParty.firebase.config
 import com.google.auth.oauth2.GoogleCredentials
 import com.google.cloud.firestore.Firestore
 import com.google.cloud.firestore.FirestoreOptions
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import java.io.FileInputStream
 
 @Configuration
-class FirebaseConfig {
+class FirebaseConfig (
+    @Value("\${firebase.key-file-name}")
+    private val keyFileName: String
+) {
 
     @Bean
     fun firestore(): Firestore {
-        val serviceAccount = FileInputStream("gsm-networking-firebase-adminsdk-rx96a-ba5984ac3f.json")
+        val serviceAccount = FileInputStream(keyFileName)
         val options = FirestoreOptions.newBuilder()
             .setCredentials(GoogleCredentials.fromStream(serviceAccount))
             .setProjectId("gsm-networking")
