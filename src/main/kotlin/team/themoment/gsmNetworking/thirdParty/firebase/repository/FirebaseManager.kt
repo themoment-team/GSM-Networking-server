b4ae0e499065f66c2cc776c89ea00ed87a30b3cb
hajeu <s22072@gsm.hs.kr>
2023-10-25 23:05:14 +0900

    :fire: 불필요한 파일 삭제

--- src/main/kotlin/team/themoment/gsmNetworking/thirdParty/firebase/repository/FirebaseManager.kt
+++
@@ -1,7 +0,0 @@
-package team.themoment.gsmNetworking.thirdParty.firebase.repository
-
-import team.themoment.gsmNetworking.domain.mentor.dto.ExistingMentorDto
-
-interface FirebaseManager {
-    fun findByUserName(userName: String): List<ExistingMentorDto>
-}
