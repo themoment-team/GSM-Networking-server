0e3803754bb14919bdbdbfa11c0d84074f805e4f
YangSiJun528 <yangsijun5528@gmail.com>
2023-10-25 15:49:36 +0900

    :sparkles: BaseChatDto 재구성

--- src/main/kotlin/team/themoment/gsmNetworking/domain/chat/dto/domain/BaseChatDto.kt
+++ src/main/kotlin/team/themoment/gsmNetworking/domain/chat/dto/domain/BaseChatDto.kt
@@ -1,16 +1,16 @@
 package team.themoment.gsmNetworking.domain.chat.dto.domain
 
-import org.springframework.data.annotation.CreatedDate
 import team.themoment.gsmNetworking.domain.chat.enums.ChatType
-import team.themoment.gsmNetworking.domain.room.domain.Room
-import java.time.LocalDateTime
-import javax.persistence.*
+import java.util.*
 
 data class BaseChatDto(
-    val id: Long,
-    val room: Room,
+    val id: UUID,
+    val room: RoomInfo,
     val content: String,
     val senderId: Long,
-    val type: ChatType,
-    var createAt: LocalDateTime
-)
+    val type: ChatType
+) {
+    data class RoomInfo(
+        val id: Long
+    )
+}
