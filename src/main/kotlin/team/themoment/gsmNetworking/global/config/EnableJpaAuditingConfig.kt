4edc79f4eec6f764eb04369eef6a88322a3c2dea
YangSiJun528 <yangsijun5528@gmail.com>
2023-10-25 15:45:43 +0900

    :fire: EnableJpaAuditingConfig 비활성화

--- src/main/kotlin/team/themoment/gsmNetworking/global/config/EnableJpaAuditingConfig.kt
+++
@@ -1,8 +0,0 @@
-package team.themoment.gsmNetworking.global.config
-
-import org.springframework.context.annotation.Configuration
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing
-
-@Configuration
-@EnableJpaAuditing
-class EnableJpaAuditingConfig
