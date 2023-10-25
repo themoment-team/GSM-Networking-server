616d31ed1670fd8a83ce4f60e59d4c51f26bec7e
YangSiJun528 <yangsijun5528@gmail.com>
2023-10-25 15:48:30 +0900

    :sparkles: RoomUserDto 재구성

--- src/main/kotlin/team/themoment/gsmNetworking/domain/room/dto/domain/RoomUserDto.kt
+++ src/main/kotlin/team/themoment/gsmNetworking/domain/room/dto/domain/RoomUserDto.kt
@@ -1,11 +1,20 @@
 package team.themoment.gsmNetworking.domain.room.dto.domain
 
-import java.time.LocalDateTime
+import team.themoment.gsmNetworking.domain.chat.enums.ChatType
+import java.util.UUID
 
 data class RoomUserDto(
     val id: Long,
     val userId: Long,
     val roomId: Long,
     val roomName: String,
-    val lastViewedTime: LocalDateTime
-)
+    val lastViewedChatId: UUID,
+    val maxChatInfo: MaxChatInfo
+) {
+    data class MaxChatInfo(
+        val id: UUID,
+        val content: String,
+        val senderId: Long,
+        val type: ChatType
+    )
+}
