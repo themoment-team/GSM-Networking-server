56834242ada5fb2a48dd52da427af295873147d6
YangSiJun528 <yangsijun5528@gmail.com>
2023-10-25 15:46:55 +0900

    :art: RecentChatQueryDto 제거

--- src/main/kotlin/team/themoment/gsmNetworking/domain/chat/dto/domain/RecentChatQueryDto.kt
+++
@@ -1,8 +0,0 @@
-package team.themoment.gsmNetworking.domain.chat.dto.domain
-
-import java.time.LocalDateTime
-
-data class RecentChatQueryDto(
-    val roomId: Long,
-    val lastViewedTime: LocalDateTime
-)
