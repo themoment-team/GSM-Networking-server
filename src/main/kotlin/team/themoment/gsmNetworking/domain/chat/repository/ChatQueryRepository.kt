5c18164319c7943b91bdbb5c4a35179387fd8f3c
YangSiJun528 <yangsijun5528@gmail.com>
2023-10-25 15:53:19 +0900

    :sparkles: ChatQueryRepository 및 구현체 재구성

--- src/main/kotlin/team/themoment/gsmNetworking/domain/chat/repository/ChatQueryRepository.kt
+++ src/main/kotlin/team/themoment/gsmNetworking/domain/chat/repository/ChatQueryRepository.kt
@@ -1,13 +1,37 @@
 package team.themoment.gsmNetworking.domain.chat.repository
 
-import team.themoment.gsmNetworking.domain.chat.domain.BaseChat
 import team.themoment.gsmNetworking.domain.chat.dto.domain.BaseChatDto
-import team.themoment.gsmNetworking.domain.chat.dto.domain.RecentChatQueryDto
 import team.themoment.gsmNetworking.domain.chat.enums.Direction
-import java.time.LocalDateTime
+import java.time.Instant
 
+/**
+ * [Chat]에 대한 쿼리 기능을 제공하는 리포지토리입니다.
+ */
 interface ChatQueryRepository {
-    fun findChatsByTimeAndDirection(roomId: Long, direction: Direction, time: LocalDateTime, limit: Long): List<BaseChat>
-    fun findRecentChats(roomId: Long, limit: Long): List<BaseChat>
-    fun findRoomsRecentChats(roomInfos: List<RecentChatQueryDto>, limit: Long): List<BaseChatDto>
+
+    /**
+     * 특정 방에서 지정된 방향과 시간을 기준으로 채팅 메시지를 검색합니다.
+     *
+     * ```
+     * time param 값에 따라 방향이 결정
+     *  - [Direction.UP]인 경우 지정된 시간 이전의 채팅을 검색 (요청에 사용된 시간 포함)
+     *  - [Direction.DOWN]인 경우 지정된 시간 이후의 채팅을 검색 (요청에 사용된 시간 포함)
+     * ```
+     *
+     * @param roomId 채팅 메시지를 검색할 방의 고유 식별자.
+     * @param direction 채팅 메시지를 검색할 방향.
+     * @param time 검색의 기준이 되는 시간.
+     * @param limit 최대 반환 개수.
+     * @return 조건에 맞는 채팅 메시지 목록. 시간순으로 내림차순으로 정렬되어 반환됩니다.
+     */
+    fun findChatsByTimeAndDirection(roomId: Long, direction: Direction, time: Instant, limit: Long): List<BaseChatDto>
+
+    /**
+     * 특정 방에서 최근 채팅 메시지를 검색합니다.
+     *
+     * @param roomId 채팅 메시지를 검색할 방의 고유 식별자.
+     * @param limit 최대 반환 개수.
+     * @return 최근 채팅 메시지 목록. 시간순으로 내림차순으로 정렬되어 반환됩니다.
+     */
+    fun findRecentChats(roomId: Long, limit: Long): List<BaseChatDto>
 }
--- src/main/kotlin/team/themoment/gsmNetworking/domain/chat/repository/ChatQueryRepositoryImpl.kt
+++ src/main/kotlin/team/themoment/gsmNetworking/domain/chat/repository/ChatQueryRepositoryImpl.kt
@@ -2,12 +2,11 @@ package team.themoment.gsmNetworking.domain.chat.repository
 
 import com.querydsl.core.types.Projections
 import com.querydsl.jpa.impl.JPAQueryFactory
-import team.themoment.gsmNetworking.domain.chat.domain.BaseChat
+import team.themoment.gsmNetworking.common.util.UUIDUtils
 import team.themoment.gsmNetworking.domain.chat.domain.QBaseChat.baseChat
 import team.themoment.gsmNetworking.domain.chat.dto.domain.BaseChatDto
 import team.themoment.gsmNetworking.domain.chat.enums.Direction
-import team.themoment.gsmNetworking.domain.chat.dto.domain.RecentChatQueryDto
-import java.time.LocalDateTime
+import java.time.*
 
 /**
  * Chat 쿼리 관련 레포지토리 [ChatQueryRepository]의 구현 클래스입니다.
@@ -19,8 +18,14 @@ class ChatQueryRepositoryImpl(
     /**
      * 특정 방에서 지정된 방향과 시간을 기준으로 채팅 메시지를 검색합니다.
      *
+     * ```
+     * time param 값에 따라 방향이 결정
+     * [Direction.UP]인 경우 지정된 시간 **이전**의 Chat를 검색 (저징된 시간 포함)
+     * [Direction.DOWN]인 경우 지정된 시간 **이후**의 Chat를 검색 (저징된 시간 포함)
+     * ```
+     *
      * @param roomId 채팅 메시지를 검색할 방의 고유 식별자.
-     * @param direction 채팅 메시지를 검색할 방향. [Direction.UP]인 경우 지정된 시간 **이전**의 Chat를 검색, [Direction.DOWN]인 경우 지정된 시간 **이후**의 Chat를 검색.
+     * @param direction 채팅 메시지를 검색할 방향.
      * @param time 검색의 기준이 되는 시간.
      * @param limit 최대 반환 개수.
      * @return 조건에 맞는 채팅 메시지 목록. 시간순으로 내림차순으로 정렬되어 반환.
@@ -28,22 +33,34 @@ class ChatQueryRepositoryImpl(
     override fun findChatsByTimeAndDirection(
         roomId: Long,
         direction: Direction,
-        time: LocalDateTime,
+        time: Instant,
         limit: Long
-    ): List<BaseChat> {
+    ): List<BaseChatDto> {
         val timeCondition = when (direction) {
-            Direction.UP -> baseChat.createAt.loe(time)
-            Direction.DOWN -> baseChat.createAt.goe(time)
+            // 입력받은 시간 + 1을 미만 조건으로 검색
+            Direction.UP -> baseChat.id.lt(UUIDUtils.generateSmallestUUIDv7(time.plusMillis(1)))
+            Direction.DOWN -> baseChat.id.goe(UUIDUtils.generateSmallestUUIDv7(time))
         }
 
         return queryFactory
-            .select(baseChat)
+            .select(
+                Projections.constructor(
+                    BaseChatDto::class.java,
+                    baseChat.id,
+                    Projections.constructor(
+                        BaseChatDto.RoomInfo::class.java,
+                        baseChat.room.id
+                    ),
+                    baseChat.content,
+                    baseChat.senderId,
+                    baseChat.type
+                )
+            )
             .from(baseChat)
             .where(baseChat.room.id.eq(roomId).and(timeCondition))
-            .orderBy(baseChat.createAt.desc(), baseChat.id.desc())
+            .orderBy(baseChat.id.desc())
             .limit(limit)
             .fetch()
-            .sortedWith(compareBy({ it.createAt }, { it.id }))
     }
 
     /**
@@ -53,33 +70,6 @@ class ChatQueryRepositoryImpl(
      * @param limit 최대 반환 개수.
      * @return 최근 채팅 메시지 목록. 시간순으로 내림차순으로 정렬되어 반환.
      */
-    override fun findRecentChats(roomId: Long, limit: Long): List<BaseChat> =
-        findChatsByTimeAndDirection(roomId, Direction.UP, LocalDateTime.MAX, limit)
-
-    /**
-     * 특정 방들의 최근 채팅 메시지를 가져옵니다.
-     *
-     * @param roomInfos
-     * @param limit 최대 반환 개수.
-     * @return 최근 채팅 메시지 목록. 시간, ID 순으로 오름차순 정렬되어 반환
-     */
-    override fun findRoomsRecentChats(roomInfos: List<RecentChatQueryDto>, limit: Long): List<BaseChatDto> {
-        return queryFactory
-            .select(
-                Projections.constructor(
-                    BaseChatDto::class.java,
-                    baseChat.id,
-                    baseChat.room,
-                    baseChat.content,
-                    baseChat.senderId,
-                    baseChat.type,
-                    baseChat.createAt
-                )
-            )
-            .from(baseChat)
-            .where(baseChat.room.id.`in`(roomInfos.map { it.roomId }))
-            .orderBy(baseChat.createAt.desc(), baseChat.createAt.desc())
-            .limit(limit)
-            .fetch()
-    }
+    override fun findRecentChats(roomId: Long, limit: Long): List<BaseChatDto> =
+        findChatsByTimeAndDirection(roomId, Direction.UP, UUIDUtils.MAX_TIME, limit)
 }
