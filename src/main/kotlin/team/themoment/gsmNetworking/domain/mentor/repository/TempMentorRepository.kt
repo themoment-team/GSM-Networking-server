f246a5e0802a6e5e4bbd2ca566818930927e9d14
hajeu <s22072@gsm.hs.kr>
2023-10-28 23:38:35 +0900

    :recycle: 콜론 뒤에 한칸 띄어쓰기

--- src/main/kotlin/team/themoment/gsmNetworking/domain/mentor/repository/TempMentorRepository.kt
+++ src/main/kotlin/team/themoment/gsmNetworking/domain/mentor/repository/TempMentorRepository.kt
@@ -3,4 +3,4 @@ package team.themoment.gsmNetworking.domain.mentor.repository
 import org.springframework.data.repository.CrudRepository
 import team.themoment.gsmNetworking.domain.mentor.domain.TempMentor
 
-interface TempMentorRepository: CrudRepository<TempMentor, Int>
+interface TempMentorRepository : CrudRepository<TempMentor, Int>
