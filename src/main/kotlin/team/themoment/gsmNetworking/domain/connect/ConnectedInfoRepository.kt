cef48734e3e0655c87ecf9b6867d6c560bc66b52
YangSiJun528 <yangsijun5528@gmail.com>
2023-10-28 23:28:43 +0900

    :sparkles:  ConnectInfo 및 Repository 구현

---
+++ src/main/kotlin/team/themoment/gsmNetworking/domain/connect/ConnectedInfoRepository.kt
@@ -0,0 +1,7 @@
+package team.themoment.gsmNetworking.domain.connect
+
+import org.springframework.data.repository.CrudRepository
+
+interface ConnectedInfoRepository : CrudRepository<ConnectInfo, String> {
+    fun findByUserId(userId: Long): ConnectInfo?
+}
