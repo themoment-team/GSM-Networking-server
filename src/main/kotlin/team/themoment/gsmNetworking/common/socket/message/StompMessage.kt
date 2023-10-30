df9917512e34eac3c943364a01fd56b7c43672c5
YangSiJun528 <yangsijun5528@gmail.com>
2023-10-30 21:11:25 +0900

    :recycle: StompMessage 에 MessageType 추가

--- src/main/kotlin/team/themoment/gsmNetworking/common/socket/message/StompMessage.kt
+++ src/main/kotlin/team/themoment/gsmNetworking/common/socket/message/StompMessage.kt
@@ -4,5 +4,11 @@ package team.themoment.gsmNetworking.common.socket.message
  * STOMP 메시지를 사용자에게 전달하기 위한 Wrapper 객체입니다.
  */
 class StompMessage<T>(
-    val content: T
-)
+    val content: T,
+    val messageType: MessageType = MessageType.MESSAGE
+) {
+    enum class MessageType {
+        ERROR,
+        MESSAGE
+    }
+}
