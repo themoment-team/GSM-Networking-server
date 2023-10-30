8b1ad1cce1e08fc40b44d1f95de399a1a920bb0e
YangSiJun528 <yangsijun5528@gmail.com>
2023-10-30 21:29:22 +0900

    :sparkles: ChatMessageController 구현

---
+++ src/main/kotlin/team/themoment/gsmNetworking/domain/chat/controller/ChatMessageController.kt
@@ -0,0 +1,69 @@
+package team.themoment.gsmNetworking.domain.chat.controller
+
+import org.springframework.messaging.handler.annotation.Header
+import org.springframework.messaging.handler.annotation.MessageMapping
+import org.springframework.messaging.handler.annotation.Payload
+import org.springframework.web.bind.annotation.RestController
+import team.themoment.gsmNetworking.common.exception.model.ErrorCode
+import team.themoment.gsmNetworking.common.socket.message.StompMessage
+import team.themoment.gsmNetworking.common.util.StompPathUtil
+import team.themoment.gsmNetworking.domain.chat.domain.BaseChat
+import team.themoment.gsmNetworking.domain.chat.dto.ws.request.UserChatRequest
+import team.themoment.gsmNetworking.domain.chat.dto.ws.response.ChatResponse
+import team.themoment.gsmNetworking.domain.chat.exception.ChatStompException
+import team.themoment.gsmNetworking.domain.chat.mapper.ChatMapper
+import team.themoment.gsmNetworking.domain.chat.mapper.RoomMapper
+import team.themoment.gsmNetworking.domain.chat.sender.ChatStompSender
+import team.themoment.gsmNetworking.domain.chat.service.ReceiveService
+import team.themoment.gsmNetworking.domain.room.domain.RoomUser
+import team.themoment.gsmNetworking.domain.room.dto.ws.RoomUserResponse
+import java.lang.IllegalArgumentException
+import java.security.Principal
+
+@RestController
+class ChatMessageController(
+    private val receiveService: ReceiveService,
+    private val chatStompSender: ChatStompSender
+) {
+
+    @MessageMapping("/chat")
+    fun sendMessage(
+        @Payload userChatRequest: UserChatRequest,
+        principal: Principal,
+        @Header("simpSessionId") sessionId: String
+    ) {
+        try {
+            val rs = receiveService.execute(principal.name.toLong(), userChatRequest)
+
+            sendChatToRoomUsers(rs.savedChat)
+            sendUpdatedRoomInfoToUsers(rs.savedChat, rs.updatedRoomUsers)
+
+        } catch (ex: IllegalArgumentException) {
+            throw ChatStompException(
+                code = ErrorCode.BAD_REQUEST,
+                message = "존재하지 않는 Room 입니다. id: ${userChatRequest.roomId}",
+                sessionId = sessionId,
+                path = "${StompPathUtil.PREFIX_TO_USER}/${sessionId}"
+            )
+        }
+    }
+
+    private fun sendChatToRoomUsers(savedChat: BaseChat) {
+        val ms: ChatResponse = ChatMapper.chatToChatResponse(savedChat)
+        val path = "${StompPathUtil.PREFIX_TO_ROOM}/${savedChat.room.id}"
+        chatStompSender.sendMessage(StompMessage(ms), path)
+    }
+
+    private fun sendUpdatedRoomInfoToUsers(
+        savedChat: BaseChat,
+        newRoomUsers: List<RoomUser>
+    ) {
+        newRoomUsers.forEach {
+            val message: RoomUserResponse = RoomMapper.chatAndRoomUserToRoomUserResponse(savedChat, it)
+            chatStompSender.sendMessageToUser(
+                StompMessage(message),
+                it.userId
+            )
+        }
+    }
+}
