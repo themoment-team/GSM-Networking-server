27ef7d9e0f75a20ddfd743368722f6d9eeafe342
YangSiJun528 <yangsijun5528@gmail.com>
2023-10-31 12:07:16 +0900

    :sparkles: `queryRecentChat`, `queryChat` 기능 구현 & api 엔드포인트 매핑
    - `queryRecentChat` : 특정 방의 최근 채팅 가져오기
    - `queryChat`: 특정 방의 채팅 특정 조건으로 가져오기

--- src/main/kotlin/team/themoment/gsmNetworking/domain/chat/controller/ChatMessageController.kt
+++ src/main/kotlin/team/themoment/gsmNetworking/domain/chat/controller/ChatMessageController.kt
@@ -8,12 +8,16 @@ import team.themoment.gsmNetworking.common.exception.model.ErrorCode
 import team.themoment.gsmNetworking.common.socket.message.StompMessage
 import team.themoment.gsmNetworking.common.util.StompPathUtil
 import team.themoment.gsmNetworking.domain.chat.domain.BaseChat
+import team.themoment.gsmNetworking.domain.chat.dto.domain.BaseChatDto
+import team.themoment.gsmNetworking.domain.chat.dto.ws.request.QueryChatRequest
+import team.themoment.gsmNetworking.domain.chat.dto.ws.request.QueryRecentChatRequest
 import team.themoment.gsmNetworking.domain.chat.dto.ws.request.UserChatRequest
 import team.themoment.gsmNetworking.domain.chat.dto.ws.response.ChatResponse
 import team.themoment.gsmNetworking.domain.chat.exception.ChatStompException
 import team.themoment.gsmNetworking.domain.chat.mapper.ChatMapper
 import team.themoment.gsmNetworking.domain.chat.mapper.RoomMapper
 import team.themoment.gsmNetworking.domain.chat.sender.ChatStompSender
+import team.themoment.gsmNetworking.domain.chat.service.QueryChatService
 import team.themoment.gsmNetworking.domain.chat.service.ReceiveService
 import team.themoment.gsmNetworking.domain.room.domain.RoomUser
 import team.themoment.gsmNetworking.domain.room.dto.ws.RoomUserResponse
@@ -23,6 +27,7 @@ import java.security.Principal
 @RestController
 class ChatMessageController(
     private val receiveService: ReceiveService,
+    private val queryChatService: QueryChatService,
     private val chatStompSender: ChatStompSender
 ) {
 
@@ -48,10 +53,54 @@ class ChatMessageController(
         }
     }
 
+    @MessageMapping("/query/recent-chats")
+    fun queryRecentChat(
+        @Payload queryRecentChatRequest: QueryRecentChatRequest,
+        @Header("simpSessionId") sessionId: String
+    ) {
+        try {
+            val rs = queryChatService.recentChats(queryRecentChatRequest)
+
+            sendChatsToSession(rs, sessionId)
+
+        } catch (ex: IllegalArgumentException) {
+            throw ChatStompException(
+                code = ErrorCode.BAD_REQUEST,
+                message = "존재하지 않는 Room 입니다. id: ${queryRecentChatRequest.roomId}",
+                sessionId = sessionId,
+                path = "${StompPathUtil.PREFIX_TO_USER}/${sessionId}"
+            )
+        }
+    }
+
+    @MessageMapping("/query/chats")
+    fun queryChat(
+        @Payload queryChatRequest: QueryChatRequest,
+        @Header("simpSessionId") sessionId: String
+    ) {
+        try {
+            val rs = queryChatService.chatsByTimeAndDirection(queryChatRequest)
+
+            sendChatsToSession(rs, sessionId)
+
+        } catch (ex: IllegalArgumentException) {
+            throw ChatStompException(
+                code = ErrorCode.BAD_REQUEST,
+                message = "존재하지 않는 Room 입니다. id: ${queryChatRequest.roomId}",
+                sessionId = sessionId,
+                path = "${StompPathUtil.PREFIX_TO_USER}/${sessionId}"
+            )
+        }
+    }
+
     private fun sendChatToRoomUsers(savedChat: BaseChat) {
         val ms: ChatResponse = ChatMapper.chatToChatResponse(savedChat)
         val path = "${StompPathUtil.PREFIX_TO_ROOM}/${savedChat.room.id}"
         chatStompSender.sendMessage(StompMessage(ms), path)
+    }
+
+    private fun sendChatsToSession(chats: List<BaseChatDto>, sessionId: String) {
+        chatStompSender.sendMessageToSession(StompMessage(chats), sessionId)
     }
 
     private fun sendUpdatedRoomInfoToUsers(
