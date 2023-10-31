c79a73cf544b30ee49b481c1495c678835db5a37
하제우 <103640011+hajeu@users.noreply.github.com>
2023-10-31 21:07:42 +0900

    이름을 받아서 TempMentor리스트를 반환하는 로직 구현 (#30)
    * 이름을 받아서 TempMentor리스트를 반환하는 로직 구현
    
    * 리뷰해주신 부분 리펙터링

--- src/main/kotlin/team/themoment/gsmNetworking/domain/mentor/dto/SearchTempMentorListDto.kt
+++ src/main/kotlin/team/themoment/gsmNetworking/domain/mentor/dto/SearchTempMentorListDto.kt
@@ -2,11 +2,11 @@ package team.themoment.gsmNetworking.domain.mentor.dto
 
 import com.fasterxml.jackson.annotation.JsonProperty
 
-data class TempMentorListDto (
-    val tempMentors: List<TempMentorInfoDto>
+data class SearchTempMentorListDto(
+    val searchTempMentors: List<SearchTempMentorInfoDto>
 )
 
-data class TempMentorInfoDto(
+data class SearchTempMentorInfoDto(
     val id: Int,
     val name: String,
     val email: String?,
--- src/main/kotlin/team/themoment/gsmNetworking/domain/mentor/service/SearchTempMentorListService.kt
+++ src/main/kotlin/team/themoment/gsmNetworking/domain/mentor/service/SearchTempMentorListService.kt
@@ -3,27 +3,28 @@ package team.themoment.gsmNetworking.domain.mentor.service
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import team.themoment.gsmNetworking.domain.mentor.dto.CompanyInfoDto
-import team.themoment.gsmNetworking.domain.mentor.dto.TempMentorInfoDto
-import team.themoment.gsmNetworking.domain.mentor.dto.TempMentorListDto
+import team.themoment.gsmNetworking.domain.mentor.dto.SearchTempMentorInfoDto
+import team.themoment.gsmNetworking.domain.mentor.dto.SearchTempMentorListDto
 import team.themoment.gsmNetworking.domain.mentor.repository.TempMentorRepository
 
 /**
- * 임시멘토의 정보를 가져오는 로직이 담긴 클래스 입니다.
+ * 임시멘토의 정보를 검색해서 가져오는 로직이 담긴 클래스 입니다.
  */
 @Service
 @Transactional(readOnly = true)
-class QueryTempMentorListService (
+class SearchTempMentorListService (
     private val tempMentorRepository: TempMentorRepository
 ) {
 
     /**
-     * 임시멘토의 정보를 가져옵니다.
+     * 임시멘토의 정보를 검색해서 가져옵니다.
      *
-     * @return 임시멘토 정보가 담긴 dto
+     * @param userName 검색할 유저의 이름
+     * @return 검색한 임시멘토 정보가 담긴 dto
      */
-    fun execute(): TempMentorListDto {
-        val tempMentors = tempMentorRepository.findAll().map { tempMentor ->
-            TempMentorInfoDto(
+    fun execute(name: String): SearchTempMentorListDto {
+        val searchTempMentors = tempMentorRepository.findByNameContaining(name).map { tempMentor ->
+            SearchTempMentorInfoDto(
                 tempMentor.id,
                 tempMentor.name,
                 tempMentor.email,
@@ -37,7 +38,6 @@ class QueryTempMentorListService (
             )
         }
 
-        return TempMentorListDto(tempMentors)
+        return SearchTempMentorListDto(searchTempMentors)
     }
-
 }
