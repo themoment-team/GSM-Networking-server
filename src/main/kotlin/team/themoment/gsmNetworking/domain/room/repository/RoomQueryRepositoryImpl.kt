a16e2093526a2e8603b68accc238a8d256631fd4
YangSiJun528 <yangsijun5528@gmail.com>
2023-11-03 11:21:30 +0900

    :sparkles: RoomQueryRepository#findRoomByUserIds 메서드 구현

--- src/main/kotlin/team/themoment/gsmNetworking/domain/room/repository/RoomQueryRepositoryImpl.kt
+++ src/main/kotlin/team/themoment/gsmNetworking/domain/room/repository/RoomQueryRepositoryImpl.kt
@@ -1,12 +1,19 @@
 package team.themoment.gsmNetworking.domain.room.repository
 
+import com.querydsl.core.group.GroupBy.groupBy
+import com.querydsl.core.group.GroupBy.list
 import com.querydsl.core.types.Projections
 import com.querydsl.core.types.dsl.BooleanExpression
+import com.querydsl.jpa.JPAExpressions
+import com.querydsl.jpa.impl.JPAQuery
 import com.querydsl.jpa.impl.JPAQueryFactory
 import team.themoment.gsmNetworking.common.util.UUIDUtils
 import team.themoment.gsmNetworking.domain.chat.domain.QBaseChat.*
+import team.themoment.gsmNetworking.domain.room.domain.QRoom.room
+import team.themoment.gsmNetworking.domain.room.domain.QRoomUser
 import team.themoment.gsmNetworking.domain.room.domain.QRoomUser.roomUser
 import team.themoment.gsmNetworking.domain.room.domain.RoomUser
+import team.themoment.gsmNetworking.domain.room.dto.domain.FetchRoomDto
 import team.themoment.gsmNetworking.domain.room.dto.domain.RoomUserDto
 import java.time.Instant
 
@@ -49,6 +56,71 @@ class RoomQueryRepositoryImpl(
      */
     override fun findRoomsByTime(userId: Long, time: Instant, size: Int): List<RoomUserDto> =
         findRooms(userId, time, size)
+
+    /**
+     * 두 사용자 사이의 채팅방을 가져옵니다.
+     *
+     * @param user1Id 사용자 1의 식별자
+     * @param user2Id 사용자 2의 식별자
+     * @return [FetchRoomDto] 반환, 조건을 만족하는 값이 없다면 Null 반환
+     */
+    override fun findRoomByUserIds(
+        user1Id: Long,
+        user2Id: Long
+    ): FetchRoomDto? {
+        // user1가 참여 중인 방 id 목록을 구하는 서브쿼리
+        val sub1 = JPAExpressions
+            .select(roomUser.room.id)
+            .from(roomUser)
+            .where(roomUser.userId.eq(user1Id))
+
+        // user2가 참여 중인 방 id 목록을 구하는 서브쿼리
+        val sub2 = JPAExpressions
+            .select(roomUser.room.id)
+            .from(roomUser)
+            .where(roomUser.userId.eq(user2Id))
+
+        // 현재 구현 상, 1:1 채팅만 지원하므로 두 사용자가 여러 방을 가질 수 없다.
+        // 그래서 해당 서브쿼리의 결과는 1 row 임이 보장되며, 메인 쿼리에서 eq 조건으로 쓰인다.
+        val unionSub = JPAExpressions
+            .select(room.id)
+            .from(room)
+            .where(
+                room.id.`in`(sub1),
+                room.id.`in`(sub2),
+            )
+
+        val rs = queryFactory
+            .selectFrom(room)
+            .where(room.id.eq(unionSub))
+            .join(room.roomUsers, roomUser)
+            .transform(
+                groupBy(room).list(
+                    Projections.constructor(
+                        FetchRoomDto::class.java,
+                        room.id,
+                        list(
+                            Projections.constructor(
+                                FetchRoomDto.RoomUserDto::class.java,
+                                roomUser.id,
+                                roomUser.userId,
+                                roomUser.roomName,
+                                roomUser.lastViewedChatId,
+                                roomUser.recentChatId
+                            )
+                        )
+                    )
+                )
+            )
+
+        return if (rs.isEmpty()) {
+            null
+        } else if (rs.size == 1) {
+            rs[0]
+        } else {
+            throw IllegalStateException("현재 구현 상, 1:1 채팅만 지원하므로 두 사용자가 여러 방을 가질 수 없습니다.")
+        }
+    }
 
     /**
      * 특정 사용자의 최근에 채팅이 발생한 방 목록 일부를 조회합니다.
