c2e50f1dba8acb3ab3f7578f6b9ab36167778077
하제우 <103640011+hajeu@users.noreply.github.com>
2023-11-07 08:03:43 +0900

    자신의 멘토 정보를 삭제하는 기능을 구현 (#41)
    * 자신의 멘토 정보를 삭제하는 기능을 구현
    
    * 메서드와 클래스명 수정
    
    * uri 변경

--- src/main/kotlin/team/themoment/gsmNetworking/domain/mentor/controller/MentorController.kt
+++ src/main/kotlin/team/themoment/gsmNetworking/domain/mentor/controller/MentorController.kt
@@ -2,13 +2,16 @@ package team.themoment.gsmNetworking.domain.mentor.controller
 
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import team.themoment.gsmNetworking.common.manager.AuthenticatedUserManager
 import team.themoment.gsmNetworking.domain.mentor.dto.MentorInfoDto
 import team.themoment.gsmNetworking.domain.mentor.dto.MentorRegistrationDto
+import team.themoment.gsmNetworking.domain.mentor.service.DeleteMyMentorInfoService
 import team.themoment.gsmNetworking.domain.mentor.service.MentorRegistrationService
 import team.themoment.gsmNetworking.domain.mentor.service.QueryAllMentorsListService
 
@@ -16,7 +19,9 @@ import team.themoment.gsmNetworking.domain.mentor.service.QueryAllMentorsListSer
 @RequestMapping("api/v1/mentor")
 class MentorController(
     private val mentorRegistrationService: MentorRegistrationService,
-    private val queryAllMentorListService: QueryAllMentorsListService
+    private val queryAllMentorListService: QueryAllMentorsListService,
+    private val deleteMyMentorInfoService: DeleteMyMentorInfoService,
+    private val authenticatedUserManager: AuthenticatedUserManager
 ) {
 
     @PostMapping
@@ -29,6 +34,12 @@ class MentorController(
     fun queryAllMentorList(): ResponseEntity<List<MentorInfoDto>> {
         val mentorList = queryAllMentorListService.execute()
         return ResponseEntity.ok(mentorList)
+    }
+
+    @DeleteMapping("/my")
+    fun deleteMyMentorInfo(): ResponseEntity<Void> {
+        deleteMyMentorInfoService.execute(authenticatedUserManager.getName())
+        return ResponseEntity.status(HttpStatus.RESET_CONTENT).build()
     }
 
 }
--- src/main/kotlin/team/themoment/gsmNetworking/global/security/SecurityConfig.kt
+++ src/main/kotlin/team/themoment/gsmNetworking/global/security/SecurityConfig.kt
@@ -64,6 +64,9 @@ class SecurityConfig(
             .mvcMatchers(HttpMethod.POST, "/api/v1/mentor").hasAnyRole(
                 Authority.TEMP_USER.name
             )
+            .mvcMatchers("/api/v1/mentor/my").hasAnyRole(
+                Authority.USER.name
+            )
             // /tempMentor
             .mvcMatchers(HttpMethod.GET, "/api/v1/temp-mentor/**").hasAnyRole(
                 Authority.TEMP_USER.name
