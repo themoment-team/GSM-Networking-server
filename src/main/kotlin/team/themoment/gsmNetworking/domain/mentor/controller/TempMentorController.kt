5fcad3fd525526c9ca1e1c915d5f7ea573e46760
hajeu <s22072@gsm.hs.kr>
2023-11-06 16:37:46 +0900

    Revert "TempMentor 삭제하는 기능 구현 (#40)"
    This reverts commit 45e70b7faee9c7a7b572734a96b69d31dc66c5a4.

--- src/main/kotlin/team/themoment/gsmNetworking/domain/mentor/controller/TempMentorController.kt
+++ src/main/kotlin/team/themoment/gsmNetworking/domain/mentor/controller/TempMentorController.kt
@@ -1,24 +1,20 @@
 package team.themoment.gsmNetworking.domain.mentor.controller
 
-import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import team.themoment.gsmNetworking.domain.mentor.dto.SearchTempMentorInfoDto
 import team.themoment.gsmNetworking.domain.mentor.dto.TempMentorInfoDto
-import team.themoment.gsmNetworking.domain.mentor.service.DeleteTempMentorService
 import team.themoment.gsmNetworking.domain.mentor.service.QueryTempMentorListService
 import team.themoment.gsmNetworking.domain.mentor.service.SearchTempMentorListService
 
 @RestController
 @RequestMapping("/api/v1/temp-mentor")
-class TempMentorController(
+class TempMentorController (
     private val queryTempMentorListService: QueryTempMentorListService,
-    private val searchTempMentorListService: SearchTempMentorListService,
-    private val deleteTempMentorService: DeleteTempMentorService
+    private val searchTempMentorListService: SearchTempMentorListService
 ) {
 
     @GetMapping
@@ -28,15 +24,9 @@ class TempMentorController(
     }
 
     @GetMapping("/{name}")
-    fun searchTempMentorListByName(@PathVariable name: String): ResponseEntity<List<SearchTempMentorInfoDto>> {
+    fun searchTempMentorListByName(@PathVariable name: String): ResponseEntity<List<SearchTempMentorInfoDto>>{
         val searchTempMentorList = searchTempMentorListService.execute(name)
         return ResponseEntity.ok(searchTempMentorList)
-    }
-
-    @DeleteMapping("/{firebaseId}")
-    fun deleteTempMentor(@PathVariable firebaseId: String): ResponseEntity<Void> {
-        deleteTempMentorService.execute(firebaseId)
-        return ResponseEntity.status(HttpStatus.RESET_CONTENT).build()
     }
 
 }
--- src/main/kotlin/team/themoment/gsmNetworking/domain/mentor/domain/TempMentor.kt
+++ src/main/kotlin/team/themoment/gsmNetworking/domain/mentor/domain/TempMentor.kt
@@ -1,12 +1,9 @@
 package team.themoment.gsmNetworking.domain.mentor.domain
 
-import org.hibernate.annotations.SQLDelete
-import org.hibernate.annotations.Where
 import javax.persistence.*
 
 @Entity
 @Table(name = "temp_mentor")
-@Where(clause = "deleted = false")
 class TempMentor(
     @Id
     @Column(name = "real_id")
@@ -34,8 +31,5 @@ class TempMentor(
     val companyName: String,
 
     @Column(nullable = false, name = "position")
-    val position: String,
-
-    @Column(nullable = false, name = "deleted")
-    var deleted: Boolean = false
+    val position: String
 )
