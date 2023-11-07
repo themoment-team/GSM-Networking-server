53dbca96206914582dbada41ade9cf06c31caaab
YangSiJun528 <yangsijun5528@gmail.com>
2023-11-07 17:35:19 +0900

    :test_tube: 요구사항에 맞게 테스트코드 명세 변경 및 추가

--- src/test/kotlin/team/themoment/gsmNetworking/domain/mentor/service/QueryAllMentorsServiceTest.kt
+++ src/test/kotlin/team/themoment/gsmNetworking/domain/mentor/service/QueryAllMentorsServiceTest.kt
@@ -42,7 +42,7 @@ class QueryAllMentorsServiceTest : BehaviorSpec({
             id = 2L,
             name = "김철수",
             email = "kim@gmail.com",
-            generation = 4,
+            generation = 3,
             position = "개발자",
             company = CompanyInfoDto(
                 name = "DEF 회사",
@@ -64,7 +64,7 @@ class QueryAllMentorsServiceTest : BehaviorSpec({
             ),
             sns = "https://www.instagram.com/c",
             profileUrl = "https://www.instagram.com/img/c",
-            registered = false
+            registered = true
         ),
         MentorInfoDto(
             id = 4L,
@@ -88,7 +88,7 @@ class QueryAllMentorsServiceTest : BehaviorSpec({
             firebaseId = "firebase_1",
             name = "임꺽정",
             email = "lim@gmail.com",
-            generation = 2,
+            generation = 3,
             position = "개발자",
             company = CompanyInfoDto(
                 name = "GHI 회사",
@@ -114,7 +114,7 @@ class QueryAllMentorsServiceTest : BehaviorSpec({
             firebaseId = "firebase_3",
             name = "정도전",
             email = "jeong@gmail.com",
-            generation = 4,
+            generation = 3,
             position = "개발자",
             company = CompanyInfoDto(
                 name = "MNO 회사",
@@ -127,7 +127,7 @@ class QueryAllMentorsServiceTest : BehaviorSpec({
             firebaseId = "firebase_4",
             name = "김유신",
             email = "kim@gmail.com",
-            generation = 3,
+            generation = 2,
             position = "개발자",
             company = CompanyInfoDto(
                 name = "PQR 회사",
@@ -137,11 +137,40 @@ class QueryAllMentorsServiceTest : BehaviorSpec({
         )
     )
 
-    given("블루 체크 사용자는 상위에 노출, 직군끼리 묶인 형태의 리스트 반환") {
+    val sameUserMentor = MentorInfoDto(
+        id = 1L,
+        name = "홍길동",
+        email = "hong@gmail.com",
+        generation = 3,
+        position = "디자이너",
+        company = CompanyInfoDto(
+            name = "ABC 회사",
+            url = "https://www.abc-company.com"
+        ),
+        sns = "https://www.instagram.com/a",
+        profileUrl = "https://www.instagram.com/img/a",
+        registered = true
+    )
+
+    val sameUserTempMentor = TempMentorInfoDto(
+        id = 14L,
+        firebaseId = "firebase_5",
+        name = "홍길동",
+        email = "hong@gmail.com",
+        generation = 3,
+        position = "디자이너",
+        company = CompanyInfoDto(
+            name = "ABC 회사",
+            url = "https://www.abc-company.com"
+        ),
+        snsUrl = "https://www.instagram.com/a"
+    )
+
+    given("블루체크 여부(true가 먼저), 직군, 기수, 이름(전부 오름차순)을 기준으로 정렬된 리스트 반환") {
         every { mentorRepository.findAllMentorInfoDto() } returns dummyMentorInfoDtos
         every { queryTempMentorListService.execute() } returns dummyTempMentorInfoDtos
 
-        `when`("임시 사용자와 블루체크 된 사용자 조회 시") {
+        `when`("조회 요청 메서드 실행 시") {
             val allMentors = queryAllMentorsService.execute()
 
             then("블루체크 된 사용자들이 반환 순서의 앞쪽에 위치한다") {
@@ -163,16 +192,38 @@ class QueryAllMentorsServiceTest : BehaviorSpec({
                 dummyTempMentorInfoDtos.forEach { it.id shouldBeIn tempUserIds }
             }
 
-            then("임시 사용자와 블루체크 된 사용자 각각 직군을 기준으로 정렬된 상태를 가진다") {
+            then("임시 사용자와 블루체크 된 사용자 각각 직군, 기수, 이름(전부 오름차순)을 기준으로 정렬된 상태를 가진다") {
                 val blueCheckedUsers = allMentors.filter { it.registered }
                 val tempUsers = allMentors.filterNot { it.registered }
 
-                val sortedBlueCheckedUsers = blueCheckedUsers.sortedBy { it.position }
-                val sortedTempUsers = tempUsers.sortedBy { it.position }
-
                 //TODO 근데 이런 식이면 구현이 드러나는 테스트 코드 아닌가? 개선이 필요해 보임
+                val sortedBlueCheckedUsers = blueCheckedUsers.sortedWith(compareBy({ it.registered }, { it.position }, { it.generation }, { it.name }))
+                val sortedTempUsers = tempUsers.sortedWith(compareBy({ it.registered }, { it.position }, { it.generation }, { it.name }))
+
                 allMentors.filter { it.registered } shouldBe sortedBlueCheckedUsers
                 allMentors.filterNot { it.registered } shouldBe sortedTempUsers
+            }
+        }
+    }
+
+    given("블루체크 된 사용자와 임시 사용자의 정보가 동일한 경우, 블루체크 된 사용자 정보만 반환") {
+        every { mentorRepository.findAllMentorInfoDto() } returns listOf(sameUserMentor)
+        every { queryTempMentorListService.execute() } returns listOf(sameUserTempMentor)
+
+        `when`("조회 요청 메서드 실행 시") {
+            val allMentors = queryAllMentorsService.execute()
+
+            then("임시 사용자와 블루체크 된 사용자 정보가 중복된다면, 블루체크 된 사용자만 반환") {
+                val blueCheckedUsers = allMentors.filter { it.registered }
+
+                blueCheckedUsers.size shouldBe 1
+
+                val firstBlueCheckedUser = blueCheckedUsers.firstOrNull()
+                firstBlueCheckedUser shouldNotBe null
+
+                firstBlueCheckedUser!!.id shouldBe sameUserMentor.id
+                firstBlueCheckedUser.name shouldBe sameUserMentor.name
+                firstBlueCheckedUser.email shouldBe sameUserMentor.email
             }
         }
     }
