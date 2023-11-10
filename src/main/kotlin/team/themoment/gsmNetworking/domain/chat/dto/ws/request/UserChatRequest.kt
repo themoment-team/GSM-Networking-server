a1353fddad0c109344ec57c9891d3c4a9791b11a
양시준 <80192911+YangSiJun528@users.noreply.github.com>
2023-11-10 23:40:44 +0900

    채팅 기능 삭제 (#56)
    * :fire: Chat 관련 기능 삭제
    
    도메인 단위부터 재구성해야 하기 때문에 코드를 삭제하였습니다.
    
    * :bug: StompSender 구현체가 없으므로 require에 실패하지 않게 init문 주석 처리

--- src/main/kotlin/team/themoment/gsmNetworking/domain/chat/dto/ws/request/UserChatRequest.kt
+++
@@ -1,6 +0,0 @@
-package team.themoment.gsmNetworking.domain.chat.dto.ws.request
-
-data class UserChatRequest(
-    val roomId: Long,
-    val content: String
-)
--- src/main/kotlin/team/themoment/gsmNetworking/domain/room/repository/RoomRepository.kt
+++
@@ -1,7 +0,0 @@
-package team.themoment.gsmNetworking.domain.room.repository
-
-import org.springframework.data.repository.CrudRepository
-import team.themoment.gsmNetworking.domain.room.domain.Room
-
-interface RoomRepository : CrudRepository<Room, Long>, RoomQueryRepository {
-}
