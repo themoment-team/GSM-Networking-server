a5f13d921f9a17818e2ed06ad3e5be3e436df2d4
하제우 <103640011+hajeu@users.noreply.github.com>
2023-11-14 14:36:13 +0900

    예외처리 되지 않은 예외를 처리 (#62)
    * 예외 처리되지 않은 예외를 처리하도록 구현
    
    * 설명 수정

--- src/main/kotlin/team/themoment/gsmNetworking/global/exception/handler/GlobalExceptionHandler.kt
+++ src/main/kotlin/team/themoment/gsmNetworking/global/exception/handler/GlobalExceptionHandler.kt
@@ -1,10 +1,16 @@
 package team.themoment.gsmNetworking.global.exception.handler
 
+import org.slf4j.LoggerFactory
+import org.springframework.http.HttpStatus
 import team.themoment.gsmNetworking.common.exception.ExpectedException
 import team.themoment.gsmNetworking.common.exception.model.ExceptionResponse
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
+import team.themoment.gsmNetworking.global.filter.LoggingFilter
+import java.lang.Exception
+
+private val log = LoggerFactory.getLogger(LoggingFilter::class.java)!!
 
 /**
  * 전역적으로 예외 클래스를 핸들링 하는 클래스 입니다.
@@ -13,15 +19,31 @@ import org.springframework.web.bind.annotation.RestControllerAdvice
 class GlobalExceptionHandler {
 
     /**
-     * ExpectedException과 ExpectedException을 상속받은 예외 클래스를 핸들해는 메서드 입니다.
+     * ExpectedException과 ExpectedException을 상속받은 예외 클래스를 핸들링하는 메서드 입니다.
      *
-     * @param e {ExpectedException 혹은 ExpectedException을 상속하는 클래스}
-     * @return ExceptionResponse
+     * @param e ExpectedException 혹은 ExpectedException을 상속하는 클래스
+     * @return ResponseEntity
      */
     @ExceptionHandler(ExpectedException::class)
-    fun handler(e: ExpectedException): ResponseEntity<ExceptionResponse> =
-        ResponseEntity
+    fun handler(e: ExpectedException): ResponseEntity<ExceptionResponse> {
+        log.warn("ExpectedException : ${e.message}")
+        log.trace("ExpectedException Details : $e")
+        return ResponseEntity
             .status(e.status)
             .body(ExceptionResponse(message = e.message))
+    }
 
+    /**
+     * 예외처리 되지 않은 RuntimeException을 포함한 하위 에러를 핸들링하는 메서드 입니다.
+     *
+     * @param e RuntimException 혹은 RuntimeException을 상속하는 클래스
+     * @return ResponseEntity
+     */
+    @ExceptionHandler(Exception::class)
+    fun unExpectedExceptionHandler(e: RuntimeException): ResponseEntity<ExceptionResponse> {
+        log.error("UnExpectedException Details : $e")
+        return ResponseEntity
+            .status(HttpStatus.INTERNAL_SERVER_ERROR)
+            .body(ExceptionResponse(message = "예외처리 되지 않은 에러가 발생하였습니다."))
+    }
 }
