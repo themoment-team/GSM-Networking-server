ed15153d33f03ac14b413cbb2988a81a46aeaac4
hajeu <s22072@gsm.hs.kr>
2023-11-17 01:11:24 +0900

    :bug: 빈 배열일 경우 예외처리

--- src/main/kotlin/team/themoment/gsmNetworking/domain/mentor/service/SearchTempMentorListService.kt
+++ src/main/kotlin/team/themoment/gsmNetworking/domain/mentor/service/SearchTempMentorListService.kt
@@ -1,7 +1,9 @@
 package team.themoment.gsmNetworking.domain.mentor.service
 
+import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
+import team.themoment.gsmNetworking.common.exception.ExpectedException
 import team.themoment.gsmNetworking.domain.mentor.dto.CompanyInfoDto
 import team.themoment.gsmNetworking.domain.mentor.dto.SearchTempMentorInfoDto
 import team.themoment.gsmNetworking.domain.mentor.repository.TempMentorRepository
@@ -37,6 +39,9 @@ class QueryTempMentorListByNameService(
             )
         }
 
-        return searchTempMentors
+        if (searchTempMentors.isEmpty())
+            throw ExpectedException("검색 결과가 없습니다.", HttpStatus.NOT_FOUND)
+        else
+            return searchTempMentors
     }
 }
