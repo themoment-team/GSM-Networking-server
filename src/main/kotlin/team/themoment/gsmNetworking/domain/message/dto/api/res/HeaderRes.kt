87730f20452823d3734477bed85abba2d6b3d7e9
YangSiJun528 <yangsijun5528@gmail.com>
2023-11-16 11:42:01 +0900

    :memo: 필요없는 TODO 주석 삭제

--- src/main/kotlin/team/themoment/gsmNetworking/domain/message/dto/api/res/HeaderRes.kt
+++ src/main/kotlin/team/themoment/gsmNetworking/domain/message/dto/api/res/HeaderRes.kt
@@ -3,7 +3,6 @@ package team.themoment.gsmNetworking.domain.message.dto.api.res
 import java.time.Instant
 import java.util.*
 
-//TODO 이름 적절하게 수정
 data class HeaderRes(
     val recentMessageId: UUID,
     val opponentUserId: Long,
