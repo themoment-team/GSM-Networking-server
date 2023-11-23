a49fb2492fbfa8893dcd04a53a5f37d2c67fda66
hajeu <s22072@gsm.hs.kr>
2023-11-24 00:16:21 +0900

    :recycle: yml time -> token

--- src/main/kotlin/team/themoment/gsmNetworking/domain/community/properties/GwangyaProperties.kt
+++ src/main/kotlin/team/themoment/gsmNetworking/domain/community/properties/GwangyaProperties.kt
@@ -4,7 +4,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.context.properties.ConstructorBinding
 
 @ConstructorBinding
-@ConfigurationProperties("gwangya.time")
+@ConfigurationProperties("gwangya.token")
 class GwangyaProperties(
     val tokenExp: Long
 )
