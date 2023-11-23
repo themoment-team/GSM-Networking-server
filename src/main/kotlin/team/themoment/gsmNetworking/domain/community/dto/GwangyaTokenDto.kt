a5866fa581e2158e42c684499764c8f213224c11
hajeu <s22072@gsm.hs.kr>
2023-11-23 23:29:43 +0900

    :sparkles: JsonFormat으로 시간 패턴 지정

--- src/main/kotlin/team/themoment/gsmNetworking/domain/community/dto/GwangyaTokenDto.kt
+++ src/main/kotlin/team/themoment/gsmNetworking/domain/community/dto/GwangyaTokenDto.kt
@@ -1,8 +1,10 @@
 package team.themoment.gsmNetworking.domain.community.dto
 
+import com.fasterxml.jackson.annotation.JsonFormat
 import java.time.LocalDateTime
 
 data class GwangyaTokenDto(
     val gwangyaToken: String,
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     val expiredTime: LocalDateTime
 )
