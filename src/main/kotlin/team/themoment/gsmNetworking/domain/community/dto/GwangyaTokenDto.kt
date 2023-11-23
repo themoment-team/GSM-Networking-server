487f850e79a1930d185733a00c623f11912a72e4
hajeu <s22072@gsm.hs.kr>
2023-11-24 01:11:00 +0900

    :recycle: 'T' 추가

--- src/main/kotlin/team/themoment/gsmNetworking/domain/community/dto/GwangyaTokenDto.kt
+++ src/main/kotlin/team/themoment/gsmNetworking/domain/community/dto/GwangyaTokenDto.kt
@@ -5,6 +5,6 @@ import java.time.LocalDateTime
 
 data class GwangyaTokenDto(
     val gwangyaToken: String,
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
     val expiredTime: LocalDateTime
 )
