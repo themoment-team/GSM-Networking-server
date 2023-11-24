911adb6d25792d21d1b4f309b75aef393284625b
hajeu <s22072@gsm.hs.kr>
2023-11-24 18:57:45 +0900

    :recycle: 스케줄러에서 서비스 객체로 변경

--- src/main/kotlin/team/themoment/gsmNetworking/domain/community/service/RenewGwangyaTokenService.kt
+++ src/main/kotlin/team/themoment/gsmNetworking/domain/community/service/RenewGwangyaTokenService.kt
@@ -1,15 +1,17 @@
-package team.themoment.gsmNetworking.domain.community.scheduler
+package team.themoment.gsmNetworking.domain.community.service
 
 import net.bytebuddy.utility.RandomString
 import org.springframework.scheduling.annotation.Scheduled
-import org.springframework.stereotype.Component
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 import team.themoment.gsmNetworking.domain.community.domain.GwangyaToken
 import team.themoment.gsmNetworking.domain.community.properties.GwangyaProperties
 import team.themoment.gsmNetworking.domain.community.repository.GwangyaTokenRepository
 import java.time.LocalDateTime
 
-@Component
-class RenewGwangyaTokenScheduler(
+@Service
+@Transactional(rollbackFor = [Exception::class])
+class RenewGwangyaTokenService(
     private val gwangyaProperties: GwangyaProperties,
     private val gwangyaTokenRepository: GwangyaTokenRepository,
 ) {
