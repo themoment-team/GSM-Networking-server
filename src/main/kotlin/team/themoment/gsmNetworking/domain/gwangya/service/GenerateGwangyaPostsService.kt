b45a2267fea1c0d764c8c880428f4b3a94ce66a7
hajeu <s22072@gsm.hs.kr>
2023-12-01 20:49:37 +0900

    :sparkles: 광야 게시물 생성 후 게시물 반환

--- src/main/kotlin/team/themoment/gsmNetworking/domain/gwangya/service/GenerateGwangyaPostsService.kt
+++ src/main/kotlin/team/themoment/gsmNetworking/domain/gwangya/service/GenerateGwangyaPostsService.kt
@@ -3,6 +3,7 @@ package team.themoment.gsmNetworking.domain.gwangya.service
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import team.themoment.gsmNetworking.domain.gwangya.domain.Gwangya
+import team.themoment.gsmNetworking.domain.gwangya.dto.GwangyaPostsDto
 import team.themoment.gsmNetworking.domain.gwangya.dto.GwangyaPostsRegistrationDto
 import team.themoment.gsmNetworking.domain.gwangya.repository.GwangyaRepository
 import java.time.LocalDateTime
@@ -13,13 +14,19 @@ class GenerateGwangyaPostsService(
     private val gwangyaRepository: GwangyaRepository
 ) {
 
-    fun execute(gwangyaPostsDto: GwangyaPostsRegistrationDto) {
+    fun execute(gwangyaPostsDto: GwangyaPostsRegistrationDto): GwangyaPostsDto {
 
         val gwangyaPosts = Gwangya(
             content = gwangyaPostsDto.content,
             createdAt = LocalDateTime.now()
         )
 
-        gwangyaRepository.save(gwangyaPosts)
+        val savedGwangyaPost = gwangyaRepository.save(gwangyaPosts)
+
+        return GwangyaPostsDto(
+            savedGwangyaPost.gwangyaId,
+            savedGwangyaPost.content,
+            savedGwangyaPost.createdAt
+        )
     }
 }
