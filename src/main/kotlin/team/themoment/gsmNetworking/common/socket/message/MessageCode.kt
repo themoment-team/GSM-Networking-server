f80f5488a2d0ba39b1d500e1f0867482bd310b92
YangSiJun528 <yangsijun5528@gmail.com>
2023-12-07 16:31:54 +0900

    :memo: KDoc 작성 및 코드 레이아웃 변경

--- src/main/kotlin/team/themoment/gsmNetworking/common/socket/message/MessageCode.kt
+++ src/main/kotlin/team/themoment/gsmNetworking/common/socket/message/MessageCode.kt
@@ -1,19 +1,28 @@
 package team.themoment.gsmNetworking.common.socket.message
 
+/**
+ * 특정 메시지 DTO와 연관된 message code를 정의하는 역할의 인터페이스.
+ *
+ * code는 구현체 간에 중복을 피하기 위해 고유하고 식별 가능한 값이어야 합니다.
+ *
+ * 구체적인 사용 예시를 확인하고 싶다면 [GlobalMessageCode]를 참고하세요.
+ */
 interface MessageCode {
 
     /**
-     * 메시지와 연관된 코드를 가져옵니다. 코드는 구현체 사이의 중복이 발생하지 않는 식별 가능한 값 이여야 합니다.
+     * 메시지와 연관된 code를 가져옵니다.
      *
-     * @return 코드
+     * code는 구현체 사이의 중복이 발생하지 않는 식별 가능한 값 이여야 합니다.
+     *
+     * @return code
      */
     val code: String
 
     /**
-     * 제공된 클래스가 메시지 코드에서 지원되는지 확인합니다.
+     * 인자로 제공된 class 지원 여부를 반환합니다.
      *
-     * @param clazz 지원 여부를 확인할 클래스입니다.
-     * @return 제공된 클래스가 지원 클래스로부터 할당 가능한 경우 `true`, 그렇지 않은 경우 `false`
+     * @param clazz
+     * @return 지원한다면 true, 아니라면 false 반환
      */
     fun isSupportClass(clazz: Class<out Any>): Boolean
 
