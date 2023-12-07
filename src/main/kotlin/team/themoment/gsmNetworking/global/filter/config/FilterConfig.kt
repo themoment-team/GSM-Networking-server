9dbee76f2c38434b4d55bf0454bb13a17cd926d5
hajeu <s22072@gsm.hs.kr>
2023-12-08 00:45:15 +0900

    :recycle: FilterConfig 주석 설명 수정

--- src/main/kotlin/team/themoment/gsmNetworking/global/filter/config/FilterConfig.kt
+++ src/main/kotlin/team/themoment/gsmNetworking/global/filter/config/FilterConfig.kt
@@ -18,13 +18,13 @@ class FilterConfig(
 
     /**
      * 커스텀 필터 순서
-     * 1. LoggingFilter
-     * 2. ExceptionHandlerFilter
-     * 3. TokenRequestFilter
-     * 4. LogoutFilter <- 커스텀 필터는 아니지만 TokenRequestFilter 뒤에 와야함
+     * 1. LoggingFilter <- 로깅을 위한 필터
+     * 2. ExceptionHandlerFilter <- 커스텀 필터들의 앞에 오지않으면 필터단에서 발생한 예외를 핸들링하지 못함
+     * 3. TokenRequestFilter <- 추출한 토큰을 이용해 사용자의 authentication 정보를 설정함
+     * 4. LogoutFilter <- 커스텀 필터는 아니지만 TokenRequestFilter 뒤에 와야 logout시 authentication 객체가 생성이 되서 옴
      */
     override fun configure(builder: HttpSecurity) {
-        //TODO 나중에 커스텀 필터를 추가하게 된다면 코드 수정이 필요함
+        //TODO 필터 순서가 명확하게 나타나도록 코드 리펙터링이 필요함
         builder.addFilterAfter(exceptionHandlerFilter, CorsFilter::class.java)
         builder.addFilterAfter(tokenRequestFilter, CorsFilter::class.java)
         builder.addFilterBefore(loggingFilter, ExceptionHandlerFilter::class.java)
