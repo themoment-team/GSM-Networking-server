85903606a1c250d66f8062b34ce9e16223717ca7
양시준 <yangsijun5528@gmail.com>
2023-12-09 21:06:36 +0900

    채팅 관련 객체 KDoc 작성 (#103)
    * :memo: KDoc 작성 및 코드 레이아웃 변경
    
    * :memo: KDoc 보완

--- src/main/kotlin/team/themoment/gsmNetworking/domain/message/service/CheckMessageService.kt
+++ src/main/kotlin/team/themoment/gsmNetworking/domain/message/service/CheckMessageService.kt
@@ -2,6 +2,18 @@ package team.themoment.gsmNetworking.domain.message.service
 
 import java.time.Instant
 
+/**
+ * 특정 사용자간의 최근 채팅 확인 시간을 갱신하는 역할의 Service.
+ */
 interface CheckMessageService {
+    /**
+     * 특정 사용자간의 최근 채팅 확인 시간을 갱신합니다.
+     *
+     * 만약 저장하려는 최근 채팅의 시간이 기존 시간보다 이전이라면 갱신되지 않습니다.
+     *
+     * @param toUserId 시간을 갱신하려는 사용자의 고유 식별자
+     * @param fromUserId 상대방 사용자의 고유 식별자
+     * @param time 변경하려는 시간
+     */
     fun execute(toUserId: Long, fromUserId: Long, time: Instant)
 }
