092511fcb7999402695a96367c4de1f55eaec2c9
양시준 <yangsijun5528@gmail.com>
2023-12-09 21:06:36 +0900

    채팅 관련 객체 KDoc 작성 (#103)
    * :memo: KDoc 작성 및 코드 레이아웃 변경
    
    * :memo: KDoc 보완

--- src/main/kotlin/team/themoment/gsmNetworking/domain/message/repository/HeaderRepository.kt
+++ src/main/kotlin/team/themoment/gsmNetworking/domain/message/repository/HeaderRepository.kt
@@ -4,6 +4,11 @@ import org.springframework.data.repository.CrudRepository
 import team.themoment.gsmNetworking.domain.message.domain.Header
 
 
+/**
+ * [Header] 엔티티를 관리하는 Repository.
+ *
+ * 복잡한 쿼리 성 메서드의 경우 [MessageRepository]에 구현되어 있습니다.
+ */
 interface HeaderRepository : CrudRepository<Header, Long> {
 
 }
