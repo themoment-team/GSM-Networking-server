68ffb6d8df67a5dbcbda6a01a1c789c87b90c2c0
hajeu <s22072@gsm.hs.kr>
2023-12-12 23:31:28 +0900

    :recycle: 리펙터링

--- src/main/kotlin/team/themoment/gsmNetworking/domain/mentor/dto/MentorSaveInfoDto.kt
+++ src/main/kotlin/team/themoment/gsmNetworking/domain/mentor/dto/MentorSaveInfoDto.kt
@@ -1,12 +1,11 @@
 package team.themoment.gsmNetworking.domain.mentor.dto
 
-import com.fasterxml.jackson.annotation.JsonProperty
 import javax.validation.constraints.NotBlank
 import javax.validation.constraints.NotEmpty
 import javax.validation.constraints.NotNull
 
 data class MentorSaveInfoDto(
-    @JsonProperty(required = true)
+    @field:NotNull
     val id: Long,
 
     @field:NotBlank
