b1f386c701dc1ecf0ba4389af50763d83fd1065b
신희성 <vvwv3092@gmail.com>
2024-03-20 20:52:18 +0900

    Feed 생성 비즈니스 로직 추가

---
+++ src/main/kotlin/team/themoment/gsmNetworking/domain/feed/service/impl/FeedService.kt
@@ -0,0 +1,38 @@
+package team.themoment.gsmNetworking.domain.feed.service.impl
+
+import org.springframework.http.HttpStatus
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+import team.themoment.gsmNetworking.common.exception.ExpectedException
+import team.themoment.gsmNetworking.common.manager.AuthenticatedUserManager
+import team.themoment.gsmNetworking.domain.feed.domain.Feed
+import team.themoment.gsmNetworking.domain.feed.dto.FeedSaveDto
+import team.themoment.gsmNetworking.domain.feed.repository.FeedRepository
+import team.themoment.gsmNetworking.domain.feed.service.GenerateFeedUseCase
+import team.themoment.gsmNetworking.domain.user.repository.UserRepository
+
+@Service
+class FeedService (
+    private val feedRepository: FeedRepository,
+    private val userRepository: UserRepository,
+    private val authenticatedUserManager: AuthenticatedUserManager
+) : GenerateFeedUseCase {
+
+    @Transactional
+    override fun generateFeed(feedSaveDto: FeedSaveDto) {
+        val authenticationId = authenticatedUserManager.getName()
+        val currentUser = userRepository.findByAuthenticationId(authenticationId)
+            ?: throw ExpectedException("유저를 찾을 수 없습니다.", HttpStatus.NOT_FOUND)
+
+        val newFeed = Feed(
+            title = feedSaveDto.title,
+            content = feedSaveDto.content,
+            category = feedSaveDto.category,
+            user = currentUser
+        )
+
+        feedRepository.save(newFeed)
+
+    }
+
+}
