6ac32a031e5bd3125730bb50c9e32470a7787be3
신희성 <vvwv3092@gmail.com>
2024-03-20 17:12:47 +0900

    Feed 엔티티 추가

---
+++ src/main/kotlin/team/themoment/gsmNetworking/domain/feed/domain/Feed.kt
@@ -0,0 +1,22 @@
+package team.themoment.gsmNetworking.domain.feed.domain
+
+import team.themoment.gsmNetworking.common.domain.BaseIdTimestampEntity
+import team.themoment.gsmNetworking.domain.user.domain.User
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.OneToOne
+import javax.persistence.Table
+
+@Entity
+@Table(name = "feed")
+class Feed (
+    @Column(name = "title")
+    val title: String,
+
+    @Column(name = "content")
+    val content: String,
+
+    @OneToOne(mappedBy = "id")
+    @Column(name = "user_id")
+    val user: User
+): BaseIdTimestampEntity();
