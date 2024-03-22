6396f491678f6d7d1c56d9c529fb2f89d71c096a
신희성 <vvwv3092@gmail.com>
2024-03-22 21:13:03 +0900

    쿼리 DLS BooleanExpression 사용

--- src/main/kotlin/team/themoment/gsmNetworking/domain/feed/repository/FeedCustomRepositoryImpl.kt
+++ src/main/kotlin/team/themoment/gsmNetworking/domain/feed/repository/FeedCustomRepositoryImpl.kt
@@ -1,6 +1,7 @@
 package team.themoment.gsmNetworking.domain.feed.repository
 
 import com.querydsl.core.types.Projections
+import com.querydsl.core.types.dsl.BooleanExpression
 import com.querydsl.jpa.impl.JPAQueryFactory
 import team.themoment.gsmNetworking.domain.feed.domain.Category
 import team.themoment.gsmNetworking.domain.feed.domain.QFeed.feed
@@ -11,76 +12,47 @@ class FeedCustomRepositoryImpl (
 ) : FeedCustomRepository {
 
     override fun findPageByCursorId(cursorId: Long, pageSize: Long, category: Category?): List<FeedInfoDto> {
-
-        if (category == null) {
-            return queryFactory.select(
-                Projections.constructor(
-                    FeedInfoDto::class.java,
-                    feed.id,
-                    feed.title,
-                    feed.category,
-                    feed.author.name,
-                    feed.createdAt
+        return queryFactory.select(
+            Projections.constructor(
+                FeedInfoDto::class.java,
+                feed.id,
+                feed.title,
+                feed.category,
+                feed.author.name,
+                feed.createdAt
                 )
             )
-                .from(feed)
-                .where(feed.id.lt(cursorId))
-                .orderBy(feed.id.desc())
-                .limit(pageSize)
-                .fetch()
-        } else {
-            return queryFactory.select(
-                Projections.constructor(
-                    FeedInfoDto::class.java,
-                    feed.id,
-                    feed.title,
-                    feed.category,
-                    feed.author.name,
-                    feed.createdAt
-                )
-            )
-                .from(feed)
-                .where(feed.id.lt(cursorId).and(feed.category.eq(category)))
-                .orderBy(feed.id.desc())
-                .limit(pageSize)
-                .fetch()
-        }
+            .from(feed)
+            .where(eqCategory(category), feed.id.lt(cursorId))
+            .orderBy(feed.id.desc())
+            .limit(pageSize)
+            .fetch()
     }
 
     override fun findPageWithRecentFeed(pageSize: Long, category: Category?): List<FeedInfoDto> {
-
-        if (category == null) {
-            return queryFactory.select(
-                Projections.constructor(
-                    FeedInfoDto::class.java,
-                    feed.id,
-                    feed.title,
-                    feed.category,
-                    feed.author.name,
-                    feed.createdAt
+        return queryFactory.select(
+            Projections.constructor(
+                FeedInfoDto::class.java,
+                feed.id,
+                feed.title,
+                feed.category,
+                feed.author.name,
+                feed.createdAt
                 )
             )
-                .from(feed)
-                .orderBy(feed.id.desc())
-                .limit(pageSize)
-                .fetch()
-        } else {
-            return queryFactory.select(
-                Projections.constructor(
-                    FeedInfoDto::class.java,
-                    feed.id,
-                    feed.title,
-                    feed.category,
-                    feed.author.name,
-                    feed.createdAt
-                )
-            )
-                .from(feed)
-                .orderBy(feed.id.desc())
-                .where(feed.category.eq(category))
-                .limit(pageSize)
-                .fetch()
+            .from(feed)
+            .orderBy(feed.id.desc())
+            .where(eqCategory(category))
+            .limit(pageSize)
+            .fetch()
         }
+
+    private fun eqCategory(category: Category?): BooleanExpression? {
+        if (category == null) {
+            return null
+        }
+
+        return feed.category.eq(category)
     }
 
 }
