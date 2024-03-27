24c5177fd8f7c5dd7ca6c4e6b74ea88d25543095
신희성 <vvwv3092@gmail.com>
2024-03-28 00:56:03 +0900

    Add QueryCommentInfoUseCase

---
+++ src/main/kotlin/team/themoment/gsmNetworking/domain/comment/dto/CommentListDto.kt
@@ -0,0 +1,8 @@
+package team.themoment.gsmNetworking.domain.comment.dto
+
+data class CommentListDto (
+    val commentId: Long,
+    val comment: String,
+    val author: Author,
+    val replies: List<Reply>
+)
---
+++ src/main/kotlin/team/themoment/gsmNetworking/domain/comment/dto/Reply.kt
@@ -0,0 +1,5 @@
+package team.themoment.gsmNetworking.domain.comment.dto
+
+data class Reply (
+    val comment: ReplyCommentInfo,
+)
