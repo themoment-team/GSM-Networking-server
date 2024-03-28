3c361588f3f06693087706592f4abeab81139123
신희성 <vvwv3092@gmail.com>
2024-03-28 20:25:59 +0900

    Update findAllByParentComment parameter

--- src/main/kotlin/team/themoment/gsmNetworking/domain/comment/repository/CommentRepository.kt
+++ src/main/kotlin/team/themoment/gsmNetworking/domain/comment/repository/CommentRepository.kt
@@ -6,6 +6,6 @@ import team.themoment.gsmNetworking.domain.comment.domain.Comment
 
 interface CommentRepository: JpaRepository<Comment, Long> {
     fun findByParentCommentAndReplyComment(parentComment: Comment, replyComment: Comment): Comment
-    fun findAllByParentComment(topComment: Comment): List<Comment>
+    fun findAllByParentComment(parentComment: Comment): List<Comment>
     fun findAllByBoardAndParentCommentIsNull(board: Board): List<Comment>
 }
