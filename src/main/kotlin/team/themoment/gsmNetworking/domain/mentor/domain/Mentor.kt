4bc4668d2d08a5dd8c87013b399548df33fcd2e5
신희성 <vvwv3092@gmail.com>
2024-04-03 16:07:57 +0900

    Add Mentor temporaryImgNumber init

--- src/main/kotlin/team/themoment/gsmNetworking/domain/mentor/domain/Mentor.kt
+++ src/main/kotlin/team/themoment/gsmNetworking/domain/mentor/domain/Mentor.kt
@@ -3,6 +3,7 @@ package team.themoment.gsmNetworking.domain.mentor.domain
 import team.themoment.gsmNetworking.common.domain.BaseIdTimestampEntity
 import team.themoment.gsmNetworking.domain.user.domain.User
 import javax.persistence.*
+import kotlin.random.Random
 
 /**
  * 멘토 정보를 저장하는 Entity 클래스 입니다.
@@ -13,11 +14,20 @@ class Mentor(
     @Column(nullable = false, name = "registered")
     val registered: Boolean,
 
-    @Column(name = "temporary_img_number")
-    val temporaryImgNumber: Int,
-
-
     @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
-    val user: User
-) : BaseIdTimestampEntity()
+    val user: User,
+
+    @Column(name = "temporary_img_number")
+    val temporaryImgNumber: Int
+) : BaseIdTimestampEntity() {
+
+    constructor(registered: Boolean, user: User) : this(registered, user, generateRandomNumber())
+
+    companion object {
+        private fun generateRandomNumber(): Int {
+            return Random.nextInt(0, 9)
+        }
+    }
+
+}
