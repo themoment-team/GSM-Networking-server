cfa175d76c9baf9df5a9b32a2d91ed7393348ef1
신희성 <vvwv3092@gmail.com>
2024-04-24 18:17:58 +0900

    Add Popup domain

---
+++ src/main/kotlin/team/themoment/gsmNetworking/domain/popup/domain/Popup.kt
@@ -0,0 +1,20 @@
+package team.themoment.gsmNetworking.domain.popup.domain
+
+import team.themoment.gsmNetworking.common.domain.BaseIdTimestampEntity
+import team.themoment.gsmNetworking.domain.board.domain.Board
+import java.time.LocalDateTime
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.OneToOne
+import javax.persistence.Table
+
+@Entity
+@Table(name = "popup")
+class Popup (
+    @OneToOne
+    @Column(name = "board_id",)
+    val board: Board,
+
+    @Column(name = "exp_time",)
+    val expTime: LocalDateTime
+): BaseIdTimestampEntity()
