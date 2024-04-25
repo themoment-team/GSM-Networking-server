6aae634d24b625b3fa45cd556b7a50216ef234d3
신희성 <vvwv3092@gmail.com>
2024-04-25 17:48:04 +0900

    Update query dsl innerjoin

--- src/main/kotlin/team/themoment/gsmNetworking/domain/popup/domain/Popup.kt
+++ src/main/kotlin/team/themoment/gsmNetworking/domain/popup/domain/Popup.kt
@@ -3,16 +3,12 @@ package team.themoment.gsmNetworking.domain.popup.domain
 import team.themoment.gsmNetworking.common.domain.BaseIdTimestampEntity
 import team.themoment.gsmNetworking.domain.board.domain.Board
 import java.time.LocalDateTime
-import javax.persistence.Column
-import javax.persistence.Entity
-import javax.persistence.JoinColumn
-import javax.persistence.OneToOne
-import javax.persistence.Table
+import javax.persistence.*
 
 @Entity
 @Table(name = "popup")
 class Popup (
-    @OneToOne
+    @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "board_id",)
     val board: Board,
 
--- src/main/kotlin/team/themoment/gsmNetworking/domain/popup/repository/PopupCustomRepositoryImpl.kt
+++ src/main/kotlin/team/themoment/gsmNetworking/domain/popup/repository/PopupCustomRepositoryImpl.kt
@@ -3,6 +3,7 @@ package team.themoment.gsmNetworking.domain.popup.repository
 import com.querydsl.core.types.Projections
 import com.querydsl.jpa.impl.JPAQueryFactory
 import team.themoment.gsmNetworking.domain.popup.domain.QPopup.popup
+import team.themoment.gsmNetworking.domain.board.domain.QBoard.board
 import team.themoment.gsmNetworking.domain.popup.dto.PopupListDto
 import java.time.LocalDateTime
 
@@ -20,6 +21,7 @@ class PopupCustomRepositoryImpl (
                 popup.board.title
             )
         ).from(popup)
+            .innerJoin(popup.board, board)
             .where(popup.expTime.gt(currentDate))
             .fetch()
     }
