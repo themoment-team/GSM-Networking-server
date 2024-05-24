ad2f5473ea7a196406b16cc8ab850a0b6664c260
hajeu <s22072@gsm.hs.kr>
2024-05-24 09:33:16 +0900

    refactor: http method 변경

--- src/main/kotlin/team/themoment/gsmNetworking/domain/board/controller/BoardController.kt
+++ src/main/kotlin/team/themoment/gsmNetworking/domain/board/controller/BoardController.kt
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -54,7 +55,7 @@ class BoardController (
         return ResponseEntity.ok(queryBoardInfoUseCase.queryBoardInfo(boardId, authenticationId))
     }
 
-    @PostMapping("/pin/{boardId}")
+    @PatchMapping("/pin/{boardId}")
     fun updatePinStatus(
         @PathVariable boardId: Long,
     ): ResponseEntity<Void> {
